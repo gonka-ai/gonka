7ef6d7b94e87ded746b7a96b3f40ff885714022f
Developer Experience team at Ignite <hello@ignite.com>
2024-07-10 17:21:41 -0700

    Initialized with Ignite CLI

---
+++ inference/x/inference/module/genesis_test.go
@@ -0,0 +1,29 @@
+package inference_test
+
+import (
+	"testing"
+
+	keepertest "github.com/productscience/inference/testutil/keeper"
+	"github.com/productscience/inference/testutil/nullify"
+	inference "github.com/productscience/inference/x/inference/module"
+	"github.com/productscience/inference/x/inference/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.InferenceKeeper(t)
+	inference.InitGenesis(ctx, k, genesisState)
+	got := inference.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
