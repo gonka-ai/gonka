707bdf1dc49fc1bae747bdd6f416bb9b20dc7dc2
dima <dima.orekhov@productscience.ai>
2024-07-17 17:54:48 -0700

    fix init script

--- inference-chain/scripts/init.sh
+++ inference-chain/scripts/init.sh
@@ -22,8 +22,8 @@ $INFERENCED_BIN keys add bob
 $INFERENCED_BIN init test --chain-id demo --default-denom "$DENOM"
 $INFERENCED_BIN config set app minimum-gas-prices "0$DENOM"
 # update genesis
-$INFERENCED_BIN genesis add-genesis-account alice 10000000mini --keyring-backend test
-$INFERENCED_BIN genesis add-genesis-account bob 1000mini --keyring-backend test
+$INFERENCED_BIN genesis add-genesis-account alice 10000000$DENOM --keyring-backend test
+$INFERENCED_BIN genesis add-genesis-account bob 1000$DENOM --keyring-backend test
 # create default validator
-$INFERENCED_BIN genesis gentx alice 1000000mini --chain-id demo
+$INFERENCED_BIN genesis gentx alice 1000000$DENOM --chain-id demo
 $INFERENCED_BIN genesis collect-gentxs
