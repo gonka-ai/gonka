cc6fe056942ceb68dfc70a843e370b196491a228
dima <dima.orekhov@productscience.ai>
2024-08-05 19:37:26 -0700

    Find where to implement EndBlock

--- inference-chain/x/inference/module/module.go
+++ inference-chain/x/inference/module/module.go
@@ -151,7 +151,11 @@ func (am AppModule) BeginBlock(_ context.Context) error {
 
 // EndBlock contains the logic that is automatically triggered at the end of each block.
 // The end block implementation is optional.
-func (am AppModule) EndBlock(_ context.Context) error {
+func (am AppModule) EndBlock(ctx context.Context) error {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	isCheckTx := sdkCtx.IsCheckTx()
+	isReCheckTx := sdkCtx.IsReCheckTx()
+	println("isCheckTx: ", isCheckTx, " isReCheckTx: ", isReCheckTx)
 	return nil
 }
 
