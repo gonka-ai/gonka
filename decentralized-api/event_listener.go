2cd988c5a1b538414d277dbd83b21e097d464f28
dima <dima.orekhov@productscience.ai>
2024-08-06 19:54:30 -0700

    Subscribe only to /inference.inference.MsgFinishInference events

--- decentralized-api/event_listener.go
+++ decentralized-api/event_listener.go
@@ -3,17 +3,20 @@ package main
 import (
 	"github.com/gorilla/websocket"
 	"log"
+	"net/url"
 )
 
 func StartEventListener(transactionRecorder InferenceCosmosClient, config Config) {
-	ws, _, err := websocket.DefaultDialer.Dial("ws://localhost:26657/websocket", nil)
+	websocketUrl := getWebsocketUrl(config)
+	log.Printf("Connecting to websocket at %s", websocketUrl)
+	ws, _, err := websocket.DefaultDialer.Dial(websocketUrl, nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer ws.Close()
 
 	// Subscribe to custom events
-	subscribeMsg := `{"jsonrpc": "2.0", "method": "subscribe", "id": "1", "params": ["tm.event='Tx'"]}`
+	subscribeMsg := `{"jsonrpc": "2.0", "method": "subscribe", "id": "1", "params": ["tm.event='Tx' AND message.action='/inference.inference.MsgFinishInference'"]}`
 	if err := ws.WriteMessage(websocket.TextMessage, []byte(subscribeMsg)); err != nil {
 		log.Fatal("write:", err)
 	}
@@ -26,4 +29,20 @@ func StartEventListener(transactionRecorder InferenceCosmosClient, config Config
 		}
 		log.Printf("Received: %s", message)
 	}
+}
+
+func getWebsocketUrl(config Config) string {
+	// Parse the input URL
+	u, err := url.Parse(config.ChainNode.Url)
+	if err != nil {
+		log.Printf("Error parsing URL: %s", err)
+		return ""
+	}
+
+	// Modify the scheme to "ws" and append the "/websocket" path
+	u.Scheme = "ws"
+	u.Path = "/websocket"
+
+	// Construct the new URL
+	return u.String()
 }
