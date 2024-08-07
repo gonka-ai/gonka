7627d48de5a68ee4615655fed75eef85cee26b53
dima <dima.orekhov@productscience.ai>
2024-08-06 20:26:11 -0700

    Test event parsing

--- decentralized-api/event_listener.go
+++ decentralized-api/event_listener.go
@@ -6,6 +6,46 @@ import (
 	"net/url"
 )
 
+type Attribute struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+	Index bool   `json:"index"`
+}
+
+type Event struct {
+	Type       string      `json:"type"`
+	Attributes []Attribute `json:"attributes"`
+}
+
+type TxResult struct {
+	Height string `json:"height"`
+	Tx     string `json:"tx"`
+	Result struct {
+		Events []Event `json:"events"`
+	} `json:"result"`
+}
+
+type Value struct {
+	TxResult TxResult `json:"TxResult"`
+}
+
+type Data struct {
+	Type  string `json:"type"`
+	Value Value  `json:"value"`
+}
+
+type Result struct {
+	Query  string              `json:"query"`
+	Data   Data                `json:"data"`
+	Events map[string][]string `json:"events"`
+}
+
+type JSONRPCResponse struct {
+	JSONRPC string `json:"jsonrpc"`
+	ID      string `json:"id"`
+	Result  Result `json:"result"`
+}
+
 func StartEventListener(transactionRecorder InferenceCosmosClient, config Config) {
 	websocketUrl := getWebsocketUrl(config)
 	log.Printf("Connecting to websocket at %s", websocketUrl)
