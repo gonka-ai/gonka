645c3fae4c4b7dbd4f9b56810e308872047f75b0
johnlong <john.long@productscience.ai>
2024-08-12 15:26:12 -0700

    Add test that uses the mock

--- inference-chain/x/inference/keeper/msg_server_start_inference_test.go
+++ inference-chain/x/inference/keeper/msg_server_start_inference_test.go
@@ -25,7 +25,8 @@ func TestMsgServer_StartInferenceWithUnregesteredParticipant(t *testing.T) {
 }
 
 func TestMsgServer_StartInference(t *testing.T) {
-	k, ms, ctx := setupMsgServer(t)
+	k, ms, ctx, mock := setupKeeperWithBankMock(t)
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
 	_, err := ms.SubmitNewParticipant(ctx, &types.MsgSubmitNewParticipant{
 		Creator: testutil.Creator,
 		Url:     "url",
@@ -37,6 +38,7 @@ func TestMsgServer_StartInference(t *testing.T) {
 		Url:     "url",
 		Models:  []string{"model1", "model2"},
 	})
+	mock.ExpectPay(sdkCtx, testutil.Requester, keeper.DefaultMaxTokens*keeper.PerTokenCost)
 	require.NoError(t, err)
 	_, err = ms.StartInference(ctx, &types.MsgStartInference{
 		InferenceId:   "inferenceId",
