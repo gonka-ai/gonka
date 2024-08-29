c823d7e5f95d1eb5baa72f55c65ef13d5ac41f1e
dima <dima.orekhov@productscience.ai>
2024-08-28 18:50:14 -0700

    Add comments

--- inference-chain/x/inference/module/chainvalidation.go
+++ inference-chain/x/inference/module/chainvalidation.go
@@ -2,4 +2,11 @@ package inference
 
 func (am AppModule) SendNewValidatorWeightsToStaking() {
 	// PRTODO: Implement
+
+	// TODO: You probably should also set new weight here for Participants?
+	//   Or should we do it as soon as we receive nonces?
+
+	// TODO: We should also delete/mark inactive any participants that failed to provide weight
+
+	// TODO: We should also delete any weights from last epochs
 }
