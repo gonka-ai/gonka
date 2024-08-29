9db2250b1df0a0ee6ca8564678b1cfd6e8e80481
dima <dima.orekhov@productscience.ai>
2024-08-28 19:01:38 -0700

    Don't do any proof of compute during stage 0

--- inference-chain/x/inference/proofofcompute/epoch.go
+++ inference-chain/x/inference/proofofcompute/epoch.go
@@ -3,18 +3,22 @@ package proofofcompute
 const epochLength = 240
 
 func IsStartOfPocStage(blockHeight int64) bool {
-	return blockHeight%epochLength == 0
+	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == 0
 }
 
 func IsEndOfPocStage(blockHeight int64) bool {
-	return blockHeight%epochLength == 60
+	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == 60
 }
 
 func IsPocExchangeWindow(startBlockHeight, currentBlockHeight int64) bool {
 	elapsedEpochs := currentBlockHeight - startBlockHeight
-	return elapsedEpochs > 0 && elapsedEpochs <= 63
+	return isNotZeroEpoch(startBlockHeight) && elapsedEpochs > 0 && elapsedEpochs <= 63
 }
 
 func IsSetNewValidatorsStage(blockHeight int64) bool {
-	return blockHeight%epochLength == 69
+	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == 69
+}
+
+func isNotZeroEpoch(blockHeight int64) bool {
+	return blockHeight >= epochLength
 }
