d18c039d27ac4e9c58bbefb9b14350780c52ad2c
dima <dima.orekhov@productscience.ai>
2024-09-04 14:25:03 -0700

    Extract constants

--- inference-chain/x/inference/proofofcompute/epoch.go
+++ inference-chain/x/inference/proofofcompute/epoch.go
@@ -1,22 +1,28 @@
 package proofofcompute
 
-const epochLength = 240
+const (
+	epochLength           = 240
+	startOfPocStage       = 0
+	endOfPocStage         = 60
+	pocExchangeDeadline   = 63
+	setNewValidatorsStage = 69
+)
 
 func IsStartOfPocStage(blockHeight int64) bool {
-	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == 0
+	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == startOfPocStage
 }
 
 func IsEndOfPocStage(blockHeight int64) bool {
-	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == 60
+	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == endOfPocStage
 }
 
 func IsPocExchangeWindow(startBlockHeight, currentBlockHeight int64) bool {
 	elapsedEpochs := currentBlockHeight - startBlockHeight
-	return isNotZeroEpoch(startBlockHeight) && elapsedEpochs > 0 && elapsedEpochs <= 63
+	return isNotZeroEpoch(startBlockHeight) && elapsedEpochs > 0 && elapsedEpochs <= pocExchangeDeadline
 }
 
 func IsSetNewValidatorsStage(blockHeight int64) bool {
-	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == 69
+	return isNotZeroEpoch(blockHeight) && blockHeight%epochLength == setNewValidatorsStage
 }
 
 func isNotZeroEpoch(blockHeight int64) bool {
