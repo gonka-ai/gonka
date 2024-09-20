5368275b6a0f26226c98623abf6244337af565d3
Tamaz Gadaev <tamaz@productscience.ai>
2024-09-20 15:15:50 +0000

    Monitor rng outputs, run race with deadline, use better rngs

--- src/pow/compute/compute.py
+++ src/pow/compute/compute.py
@@ -1,6 +1,8 @@
 import hashlib
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from abc import (
     ABC,
     abstractmethod,
@@ -9,70 +11,84 @@ from typing import Callable
 
 
 
-class BaseCompute(ABC):
-    def __init__(
-        self,
-        public_key: str,
-    ):
-        self.public_key = public_key
-        self.compute_function = self.get_compute_function()
+# class BaseCompute(ABC):
+#     def __init__(
+#         self,
+#         public_key: str,
+#         device,
+#     ):
+#         self.public_key = public_key
+#         self.device = device
 
+
+#     def __call__(self, nonce: int) -> bytes:
+#         input_tensor = self.get_input_tensor(nonce)
+#         output_tensor = self.model(input_tensor)
+#         hash_ = self.get_hash(output_tensor)
+#         return hash_
+
+
+#     @abstractmethod
+#     def get_input_tensor(
+#         self,
+#         nonce: int,
+#     ) -> torch.Tensor:
+#         pass
+
+
+#     @abstractmethod
+#     def get_hash(self, tensor: torch.Tensor) -> bytes:
+#         pass
+
+
+#     @abstractmethod
+#     def get_compute_function(self) -> Callable[[torch.Tensor], torch.Tensor]:
+#         pass
+
+
+class AttentionModel(torch.nn.Module):
+    def __init__(self, K, V):
+        super(AttentionModel, self).__init__()
+        self.K = K
+        self.V = V
+
+    def forward(self, Q: torch.Tensor) -> torch.Tensor:
+        scores = torch.matmul(Q, self.K.transpose(-2, -1)) / torch.sqrt(torch.tensor(Q.shape[1], dtype=torch.float32))
+        weights = F.softmax(scores, dim=-1)
+        R = torch.matmul(weights, self.V)
+        return R
+
+
+class Compute:
+    def __init__(self, public_key: str, device, hid):
+        self.public_key = public_key
+        self.device = device
+        self.hid = hid
+        self.rng = torch.Generator(device=device)
+        self.model = self.get_compute_function()
+        
+
+    def get_input_tensor(self, nonce: int) -> torch.Tensor:
+        seed = (hash(self.public_key) + nonce) % (2**32)
+        self.rng.manual_seed(seed)
+        Q = torch.randn(size=(self.hid, self.hid), generator=self.rng, device=self.device, dtype=torch.float32)
+        return Q
+
+    def get_hash(self, tensor: torch.Tensor) -> bytes:
+        if self.device.type == 'cuda':
+            values = tensor.cpu().numpy()
+        else:
+            values = tensor.numpy()
+        result_hash = hashlib.sha256(values.tobytes()).digest()
+        return result_hash
+
+    def get_compute_function(self) -> Callable[[torch.Tensor], torch.Tensor]:
+        K = torch.randn(size=(self.hid, self.hid), generator=self.rng, device=self.device, dtype=torch.float32)
+        V = torch.randn(size=(self.hid, self.hid), generator=self.rng, device=self.device, dtype=torch.float32)
+        return AttentionModel(K, V).to(self.device)
 
     def __call__(self, nonce: int) -> bytes:
         input_tensor = self.get_input_tensor(nonce)
         output_tensor = self.model(input_tensor)
         hash_ = self.get_hash(output_tensor)
         return hash_
-
-
-    @abstractmethod
-    def get_input_tensor(
-        self,
-        nonce: int,
-    ) -> torch.Tensor:
-        pass
-
-
-    @abstractmethod
-    def get_hash(self, tensor: torch.Tensor) -> bytes:
-        pass
-
-
-    @abstractmethod
-    def get_compute_function(self) -> Callable[[torch.Tensor], torch.Tensor]:
-        pass
-
-
-class AttentionModel(torch.nn.Module):
-    def __init__(self):
-        super(AttentionModel, self).__init__()
-        np.random.seed(42)
-        torch.manual_seed(42)
-        self.K = torch.rand((10, 10), dtype=torch.float32)
-        self.V = torch.rand((10, 10), dtype=torch.float32)
-
-    def forward(self, Q: torch.Tensor) -> torch.Tensor:
-        scores = torch.matmul(Q, self.K.transpose(-2, -1)) / torch.sqrt(torch.tensor(Q.shape[1], dtype=torch.float32))
-        weights = torch.nn.functional.softmax(scores, dim=-1)
-        R = torch.matmul(weights, self.V)
-        return R
-
-
-class Compute(BaseCompute):
-    def __init__(self, public_key: str):
-        super().__init__(public_key)
-        self.model = self.get_compute_function()
-
-    def get_input_tensor(self, nonce: int) -> torch.Tensor:
-        seed = (hash(self.public_key) + nonce) % (2**32)
-        np.random.seed(seed)
-        Q = np.random.rand(10, 10)
-        return torch.tensor(Q, dtype=torch.float32)
-
-    def get_hash(self, tensor: torch.Tensor) -> bytes:
-        result_hash = hashlib.sha256(tensor.numpy().tobytes()).digest()
-        return result_hash
-
-
-    def get_compute_function(self) -> Callable[[torch.Tensor], torch.Tensor]:
-        return AttentionModel()
--- src/pow/compute/pipeline.py
+++ src/pow/compute/pipeline.py
@@ -1,29 +1,37 @@
+import datetime
+from tqdm import tqdm
 from typing import Tuple
 from abc import ABC
 from pow.compute.utils import meets_required_zeros
-from pow.compute.compute import BaseCompute
+from pow.compute.compute import Compute
 
 
 class Pipeline(ABC):
     def __init__(
         self,
         public_key: str,
-        compute: BaseCompute,
+        compute: Compute,
         target_leading_zeros: int,
     ):
         self.public_key = public_key
         self.compute = compute
         self.nonce = 0
         self.target_leading_zeros = target_leading_zeros
+        self.proof = []
 
-    def iter(self) -> Tuple[bytes, int]:
+    def race(self, race_duration) -> Tuple[bytes, int]:
         self.nonce = 0
-        while True:
-            self.nonce = self.get_next_nonce()
-            hash_ = self.compute(self.nonce)
-            if meets_required_zeros(hash_, self.target_leading_zeros):
-                return (hash_, self.nonce)
-
+        start_time = datetime.datetime.now()
+        time_passed = datetime.datetime.now() - start_time
+        with tqdm(total=race_duration, desc='Time passed') as pbar:
+            while time_passed.seconds < race_duration:
+                self.nonce = self.get_next_nonce()
+                hash_ = self.compute(self.nonce)
+                if meets_required_zeros(hash_, self.target_leading_zeros):
+                    self.proof.append((hash_, self.nonce))
+                time_passed = datetime.datetime.now() - start_time
+                pbar.update(time_passed.seconds - pbar.n)
+        
 
     def get_next_nonce(self):
         return self.nonce + 1
