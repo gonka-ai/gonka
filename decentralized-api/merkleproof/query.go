c95d5b40fc6a1faaabe9572f052e2b2804111fbc
dima <dima.orekhov@productscience.ai>
2024-09-23 11:50:34 -0600

    Sketch query with proof function

---
+++ decentralized-api/merkleproof/query.go
@@ -0,0 +1,47 @@
+package merkleproof
+
+import (
+	"context"
+	"fmt"
+	"github.com/cometbft/cometbft/rpc/core/types"
+
+	rpcclient "github.com/cometbft/cometbft/rpc/client"
+	"github.com/cometbft/cometbft/rpc/client/http"
+)
+
+func QueryWithProof(storeKey, dataKey string) *coretypes.ResultABCIQuery {
+	// Create a new RPC client
+	rpcClient, err := http.New("http://localhost:26657", "/websocket")
+	if err != nil {
+		panic(err)
+	}
+
+	key := []byte(dataKey)
+	path := fmt.Sprintf("store/%s/key", storeKey)
+
+	// Perform the ABCI query with proof enabled
+	result, err := rpcClient.ABCIQueryWithOptions(context.Background(), path, key, rpcclient.ABCIQueryOptions{Prove: true})
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
+
+/*func VerifyProof(proofOps *cryptotypes.ProofOps, key, value, appHash []byte) error {
+	// Convert ProofOps to Merkle proof
+	merkleProof, err := merkle.ProofFromProto(proofOps.Ops[0].GetData())
+	if err != nil {
+		return err
+	}
+
+	// Compute the root hash from the proof
+	rootHash := merkleProof.ComputeRootHash(key, value)
+
+	// Compare the computed root hash with the app hash
+	if !bytes.Equal(rootHash, appHash) {
+		return fmt.Errorf("computed root hash does not match app hash")
+	}
+
+	return nil
+}*/
