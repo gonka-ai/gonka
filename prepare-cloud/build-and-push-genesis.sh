8d8e547f1602cbccc5dbd5846c14e017e8b92986
dima <dima.orekhov@productscience.ai>
2024-10-11 14:45:52 -0700

    FIXME

--- prepare-cloud/build-and-push-genesis.sh
+++ prepare-cloud/build-and-push-genesis.sh
@@ -1,5 +1,5 @@
 make -C ../. node-build-genesis
 make -C ../inference-chain docker-push-genesis
 
-make -C ../. node-release-docker
-make -C ../inference-chain docker-push-join
+make -C ../. api-release-docker
+make -C ../decentralized-api docker-push-release
