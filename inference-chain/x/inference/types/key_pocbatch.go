cba2ede5ae64ac246850b92e20c90c46c850965c
dima <dima.orekhov@productscience.ai>
2024-11-14 22:25:56 +0100

    Key function

---
+++ inference-chain/x/inference/types/key_pocbatch.go
@@ -0,0 +1,18 @@
+package types
+
+import "strconv"
+
+const PocBatchKeyPrefix = "PoCBatch/value/"
+
+func PoCBatchKey(pocStageStartBlockHeight int64, participantIndex string, batchId string) []byte {
+	var key []byte
+
+	key = append(key, []byte(strconv.FormatInt(pocStageStartBlockHeight, 10))...)
+	key = append(key, []byte("/")...)
+	key = append(key, []byte(participantIndex)...)
+	key = append(key, []byte("/")...)
+	key = append(key, []byte(batchId)...)
+	key = append(key, []byte("/")...)
+
+	return key
+}
