09aeb1b102188d3f29a440a076342b495603ad3f
johnlong <john.long@productscience.ai>
2024-12-06 11:34:50 -0800

    Refactor for clarity

--- inference-chain/x/inference/keeper/msg_server_claim_rewards.go
+++ inference-chain/x/inference/keeper/msg_server_claim_rewards.go
@@ -13,27 +13,9 @@ import (
 func (k msgServer) ClaimRewards(goCtx context.Context, msg *types.MsgClaimRewards) (*types.MsgClaimRewardsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	settleAmount, found := k.GetSettleAmount(ctx, msg.Creator)
-	if !found {
-		k.LogDebug("SettleAmount not found for address", "address", msg.Creator)
-		return &types.MsgClaimRewardsResponse{
-			Amount: 0,
-			Result: "No rewards for this address",
-		}, nil
-	}
-	if settleAmount.PocStartHeight != msg.PocStartHeight {
-		k.LogDebug("SettleAmount does not match height", "height", msg.PocStartHeight, "settleHeight", settleAmount.PocStartHeight)
-		return &types.MsgClaimRewardsResponse{
-			Amount: 0,
-			Result: "No rewards for this block height",
-		}, nil
-	}
-	if settleAmount.GetTotalCoins() == 0 {
-		k.LogDebug("SettleAmount had zero coins", "address", msg.Creator)
-		return &types.MsgClaimRewardsResponse{
-			Amount: 0,
-			Result: "No rewards for this address",
-		}, nil
+	settleAmount, response := k.validateRequest(ctx, msg)
+	if response != nil {
+		return response, nil
 	}
 
 	err := k.validateClaim(ctx, msg, settleAmount)
@@ -42,68 +24,67 @@ func (k msgServer) ClaimRewards(goCtx context.Context, msg *types.MsgClaimReward
 	}
 	k.LogDebug("Claim verified", "account", msg.Creator, "seed", msg.Seed)
 
-	totalCoins := settleAmount.GetTotalCoins()
-	k.LogInfo("Issuing rewards", "address", msg.Creator, "amount", totalCoins)
-	err = k.PayParticipantFromEscrow(ctx, msg.Creator, totalCoins)
+	err = k.payoutClaim(ctx, msg, settleAmount)
 	if err != nil {
-		k.LogError("Error paying participant", "error", err)
-		// Big question: do we remove the settle amount? Probably not
 		return nil, err
 	}
-	k.RemoveSettleAmount(ctx, msg.Creator)
 
 	return &types.MsgClaimRewardsResponse{
-		Amount: totalCoins,
+		Amount: settleAmount.GetTotalCoins(),
 		Result: "Rewards claimed",
 	}, nil
 }
 
-func (k msgServer) validateClaim(ctx sdk.Context, msg *types.MsgClaimRewards, settleAmount types.SettleAmount) error {
-	k.LogInfo("Validating claim", "account", msg.Creator, "seed", msg.Seed, "height", msg.PocStartHeight)
-	addr, err := sdk.AccAddressFromBech32(msg.Creator)
+func (ms msgServer) payoutClaim(ctx sdk.Context, msg *types.MsgClaimRewards, settleAmount *types.SettleAmount) error {
+	ms.LogInfo("Issuing rewards", "address", msg.Creator, "amount", settleAmount.GetTotalCoins())
+	err := ms.PayParticipantFromEscrow(ctx, msg.Creator, settleAmount.GetTotalCoins())
 	if err != nil {
-		return types.ErrPocAddressInvalid
+		ms.LogError("Error paying participant", "error", err)
+		return err
 	}
-	acc := k.AccountKeeper.GetAccount(ctx, addr)
+	ms.RemoveSettleAmount(ctx, msg.Creator)
+	return nil
+}
 
+func (k msgServer) validateRequest(ctx sdk.Context, msg *types.MsgClaimRewards) (*types.SettleAmount, *types.MsgClaimRewardsResponse) {
+	settleAmount, found := k.GetSettleAmount(ctx, msg.Creator)
+	if !found {
+		k.LogDebug("SettleAmount not found for address", "address", msg.Creator)
+		return nil, &types.MsgClaimRewardsResponse{
+			Amount: 0,
+			Result: "No rewards for this address",
+		}
+	}
 	if settleAmount.PocStartHeight != msg.PocStartHeight {
-		k.LogError("Claim rewards height mismatch", "expected", settleAmount.PocStartHeight, "actual", msg.PocStartHeight)
-		return types.ErrParticipantNotFound
+		k.LogDebug("SettleAmount does not match height", "height", msg.PocStartHeight, "settleHeight", settleAmount.PocStartHeight)
+		return nil, &types.MsgClaimRewardsResponse{
+			Amount: 0,
+			Result: "No rewards for this block height",
+		}
 	}
-	pubKey := acc.GetPubKey()
-	seedBytes := make([]byte, 8)
-	binary.BigEndian.PutUint64(seedBytes, uint64(msg.Seed))
-	signature, err := hex.DecodeString(settleAmount.SeedSignature)
-	if err != nil {
-		k.LogError("Error decoding signature", "error", err)
-		return err
-	}
-	k.LogDebug("Verifying signature", "seedBytes", hex.EncodeToString(seedBytes), "signature", hex.EncodeToString(signature), "pubkey", pubKey.String())
-	if !pubKey.VerifySignature(seedBytes, signature) {
-		k.LogError("Signature verification failed", "seed", msg.Seed, "signature", settleAmount.SeedSignature, "seedBytes", hex.EncodeToString(seedBytes))
-		return types.ErrClaimSignatureInvalid
-	}
-	epochData, found := k.GetEpochGroupData(ctx, msg.PocStartHeight)
-	if !found {
-		k.LogError("Epoch data not found", "height", msg.PocStartHeight)
-		return types.ErrCurrentEpochGroupNotFound
-	}
-	mustBeValidated, err := k.getMustBeValidatedInferences(epochData, msg)
-	if err != nil {
-		return err
-	}
-	wasValidatedRaw, found := k.GetEpochGroupValidations(ctx, msg.Creator, msg.PocStartHeight)
-	if !found {
-		k.LogInfo("Validations not found", "height", msg.PocStartHeight, "account", msg.Creator)
-		wasValidatedRaw = types.EpochGroupValidations{
-			ValidatedInferences: make([]string, 0),
+	if settleAmount.GetTotalCoins() == 0 {
+		k.LogDebug("SettleAmount had zero coins", "address", msg.Creator)
+		return nil, &types.MsgClaimRewardsResponse{
+			Amount: 0,
+			Result: "No rewards for this address",
 		}
 	}
 
-	wasValidated := make(map[string]bool)
-	for _, inferenceId := range wasValidatedRaw.ValidatedInferences {
-		wasValidated[inferenceId] = true
+	return &settleAmount, nil
+}
+
+func (k msgServer) validateClaim(ctx sdk.Context, msg *types.MsgClaimRewards, settleAmount *types.SettleAmount) error {
+	k.LogInfo("Validating claim", "account", msg.Creator, "seed", msg.Seed, "height", msg.PocStartHeight)
+	err := k.validateSeedSignature(ctx, msg, settleAmount)
+	if err != nil {
+		return err
 	}
+
+	mustBeValidated, err := k.getMustBeValidatedInferences(ctx, msg)
+	if err != nil {
+		return err
+	}
+	wasValidated := k.getValidatedInferences(ctx, msg)
 
 	validationMissed := false
 
@@ -120,7 +101,56 @@ func (k msgServer) validateClaim(ctx sdk.Context, msg *types.MsgClaimRewards, se
 	return nil
 }
 
-func (k msgServer) getMustBeValidatedInferences(epochData types.EpochGroupData, msg *types.MsgClaimRewards) ([]string, error) {
+func (ms msgServer) validateSeedSignature(ctx sdk.Context, msg *types.MsgClaimRewards, settleAmount *types.SettleAmount) error {
+	ms.LogDebug("Validating seed signature", "account", msg.Creator, "seed", msg.Seed, "height", msg.PocStartHeight)
+	addr, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return types.ErrPocAddressInvalid
+	}
+	acc := ms.AccountKeeper.GetAccount(ctx, addr)
+	if acc == nil {
+		ms.LogError("Account not found for signature", "address", msg.Creator)
+		return types.ErrParticipantNotFound
+	}
+	pubKey := acc.GetPubKey()
+	seedBytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(seedBytes, uint64(msg.Seed))
+	signature, err := hex.DecodeString(settleAmount.SeedSignature)
+	if err != nil {
+		ms.LogError("Error decoding signature", "error", err)
+		return err
+	}
+	ms.LogDebug("Verifying signature", "seedBytes", hex.EncodeToString(seedBytes), "signature", hex.EncodeToString(signature), "pubkey", pubKey.String())
+	if !pubKey.VerifySignature(seedBytes, signature) {
+		ms.LogError("Signature verification failed", "seed", msg.Seed, "signature", settleAmount.SeedSignature, "seedBytes", hex.EncodeToString(seedBytes))
+		return types.ErrClaimSignatureInvalid
+	}
+	return nil
+}
+
+func (k msgServer) getValidatedInferences(ctx sdk.Context, msg *types.MsgClaimRewards) map[string]bool {
+	wasValidatedRaw, found := k.GetEpochGroupValidations(ctx, msg.Creator, msg.PocStartHeight)
+	if !found {
+		k.LogInfo("Validations not found", "height", msg.PocStartHeight, "account", msg.Creator)
+		wasValidatedRaw = types.EpochGroupValidations{
+			ValidatedInferences: make([]string, 0),
+		}
+	}
+
+	wasValidated := make(map[string]bool)
+	for _, inferenceId := range wasValidatedRaw.ValidatedInferences {
+		wasValidated[inferenceId] = true
+	}
+	return wasValidated
+}
+
+func (k msgServer) getMustBeValidatedInferences(ctx sdk.Context, msg *types.MsgClaimRewards) ([]string, error) {
+	epochData, found := k.GetEpochGroupData(ctx, msg.PocStartHeight)
+	if !found {
+		k.LogError("Epoch data not found", "height", msg.PocStartHeight)
+		return nil, types.ErrCurrentEpochGroupNotFound
+	}
+
 	totalWeight := int64(0)
 	weightMap := make(map[string]int64)
 	for _, weight := range epochData.ValidationWeights {
