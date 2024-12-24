ff196d8eaffd721d566cf6fff1c3cc9a6f69f476
dima <dima.orekhov@productscience.ai>
2024-12-24 00:33:42 -0800

    Scaffold submit seed message

---
+++ inference-chain/x/inference/keeper/msg_server_submit_seed.go
@@ -0,0 +1,17 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/productscience/inference/x/inference/types"
+)
+
+func (k msgServer) SubmitSeed(goCtx context.Context, msg *types.MsgSubmitSeed) (*types.MsgSubmitSeedResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	return &types.MsgSubmitSeedResponse{}, nil
+}
---
+++ inference-chain/x/inference/types/message_submit_seed.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+var _ sdk.Msg = &MsgSubmitSeed{}
+
+func NewMsgSubmitSeed(creator string, seed int32, height int32, signature string) *MsgSubmitSeed {
+	return &MsgSubmitSeed{
+		Creator:     creator,
+		Seed:        seed,
+		BlockHeight: height,
+		Signature:   signature,
+	}
+}
+
+func (msg *MsgSubmitSeed) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	return nil
+}
