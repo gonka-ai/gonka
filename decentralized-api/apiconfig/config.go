835bad5a278cfcade103b7245f6de5ec2acfee7c
dima <dima.orekhov@productscience.ai>
2025-01-06 22:57:58 -0800

    Don't use sed

--- decentralized-api/apiconfig/config.go
+++ decentralized-api/apiconfig/config.go
@@ -49,7 +49,33 @@ func ReadConfig() Config {
 	if err != nil {
 		log.Fatalf("error unmarshalling config: %v", err)
 	}
+
+	setEnvVars(&config)
+
 	return config
+}
+
+func setEnvVars(config *Config) {
+	if keyName, found := os.LookupEnv("KEY_NAME"); found {
+		slog.Info("Setting config.ChainNode.AccountName to env var", "AccountName", keyName)
+		config.ChainNode.AccountName = keyName
+	} else {
+		slog.Warn("KEY_NAME not set. Config value will be used", "AccountName", config.ChainNode.AccountName)
+	}
+
+	if pocCallbackUrl, found := os.LookupEnv("POC_CALLBACK_URL"); found {
+		slog.Info("Setting config.Api.PoCCallbackUrl to env var", "PoCCallbackUrl", pocCallbackUrl)
+		config.Api.PoCCallbackUrl = pocCallbackUrl
+	} else {
+		slog.Warn("POC_CALLBACK_URL not set. Config value will be used", "PoCCallbackUrl", config.Api.PoCCallbackUrl)
+	}
+
+	if publicUrl, found := os.LookupEnv("PUBLIC_URL"); found {
+		slog.Info("Setting config.Api.PublicUrl to env var", "PublicUrl", publicUrl)
+		config.Api.PublicUrl = publicUrl
+	} else {
+		slog.Warn("PUBLIC_URL not set. Config value will be used", "PublicUrl", config.Api.PublicUrl)
+	}
 }
 
 func WriteConfig(config Config) error {
--- decentralized-api/scripts/init-docker.sh
+++ decentralized-api/scripts/init-docker.sh
@@ -11,6 +11,11 @@ if [ -z "$POC_CALLBACK_URL" ]; then
   exit 1
 fi
 
+if [ -z "$PUBLIC_URL" ]; then
+  echo "Error: PUBLIC_URL is required."
+  exit 1
+fi
+
 yaml_file="/root/api-config.yaml"
 
 if [ -n "$NODE_HOST" ]; then
@@ -18,14 +23,13 @@ if [ -n "$NODE_HOST" ]; then
   sed -i "s/url: .*:26657/url: http:\/\/$NODE_HOST:26657/" "$yaml_file"
 fi
 
-echo "Setting account_name address to $KEY_NAME in $yaml_file"
-sed -i "s/account_name: .*/account_name: \"$KEY_NAME\"/" "$yaml_file"
-
 echo "Setting keyring_backend to test in $yaml_file"
 sed -i "s/keyring_backend: .*/keyring_backend: test/" "$yaml_file"
 
-echo "Setting poc_callback_url to $POC_CALLBACK_URL in $yaml_file"
-sed -i "s/poc_callback_url: .*/poc_callback_url: \"$POC_CALLBACK_URL\"/" "$yaml_file"
+if [ "$IS_GENESIS" = "true" ]; then
+  echo "Setting IS_GENESIS to $IS_GENESIS in $yaml_file"
+  sed -i "s/is_genesis: .*/is_genesis: \"$IS_GENESIS\"/" "$yaml_file"
+fi
 
 echo "The final api config:"
 cat "$yaml_file"
