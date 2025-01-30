d430ab977391789fb5dd4f813e703325a94af50f
Gleb Morgachev <morgachev.g@gmail.com>
2025-01-29 18:48:26 -0800

    Smth with WASM

--- inference-chain/app/app.go
+++ inference-chain/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -19,6 +20,7 @@ import (
 	_ "cosmossdk.io/x/nft/module" // import for side-effects
 	_ "cosmossdk.io/x/upgrade"    // import for side-effects
 	upgradekeeper "cosmossdk.io/x/upgrade/keeper"
+
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -76,9 +78,11 @@ import (
 	ibckeeper "github.com/cosmos/ibc-go/v8/modules/core/keeper"
 
 	inferencemodulekeeper "github.com/productscience/inference/x/inference/keeper"
-	// this line is used by starport scaffolding # stargate/app/moduleImport
+	// starport scaffolding # stargate/app/moduleImport
 
+	// WASM
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
 	"github.com/productscience/inference/docs"
 )
@@ -88,23 +92,16 @@ const (
 	Name                 = "inference"
 )
 
-var (
-	wasmKeeper wasmkeeper.Keeper
-)
+// DefaultNodeHome default home directory for the daemon
+var DefaultNodeHome string
 
-var (
-	// DefaultNodeHome default home directories for the application daemon
-	DefaultNodeHome string
-)
-
+// These variables allow your code to satisfy runtime.AppI and servertypes.Application
 var (
 	_ runtime.AppI            = (*App)(nil)
 	_ servertypes.Application = (*App)(nil)
 )
 
-// App extends an ABCI application, but with most of its parameters exported.
-// They are exported for convenience in creating helper functions, as object
-// capabilities aren't needed for testing.
+// App is your ABCI application
 type App struct {
 	*runtime.App
 	legacyAmino       *codec.LegacyAmino
@@ -133,7 +130,7 @@ type App struct {
 	CircuitBreakerKeeper circuitkeeper.Keeper
 
 	// IBC
-	IBCKeeper           *ibckeeper.Keeper // IBC Keeper must be a pointer in the app, so we can SetRouter on it correctly
+	IBCKeeper           *ibckeeper.Keeper
 	CapabilityKeeper    *capabilitykeeper.Keeper
 	IBCFeeKeeper        ibcfeekeeper.Keeper
 	ICAControllerKeeper icacontrollerkeeper.Keeper
@@ -146,10 +143,14 @@ type App struct {
 	ScopedICAControllerKeeper capabilitykeeper.ScopedKeeper
 	ScopedICAHostKeeper       capabilitykeeper.ScopedKeeper
 
+	// custom module keepers
 	InferenceKeeper inferencemodulekeeper.Keeper
-	// this line is used by starport scaffolding # stargate/app/keeperDeclaration
+	// starport scaffolding # stargate/app/keeperDeclaration
 
-	// simulation manager
+	// Manually added WASM keeper
+	WasmKeeper wasmkeeper.Keeper
+
+	// Simulation manager
 	sm *module.SimulationManager
 }
 
@@ -158,41 +159,76 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-
 	DefaultNodeHome = filepath.Join(userHomeDir, "."+Name)
 }
 
-// getGovProposalHandlers return the chain proposal handlers.
+// getGovProposalHandlers returns the chain's proposal handlers
 func getGovProposalHandlers() []govclient.ProposalHandler {
 	var govProposalHandlers []govclient.ProposalHandler
-	// this line is used by starport scaffolding # stargate/app/govProposalHandlers
-
-	govProposalHandlers = append(govProposalHandlers,
+	govProposalHandlers = append(
+		govProposalHandlers,
 		paramsclient.ProposalHandler,
-		// this line is used by starport scaffolding # stargate/app/govProposalHandler
 	)
-
 	return govProposalHandlers
 }
 
-// AppConfig returns the default app config.
+// ProvideWasmKeeper manually constructs the WASM keeper with minimal IBC/distribution
+// (passed as nil), so it compiles but won't enable advanced features in WASM.
+func ProvideWasmKeeper(app *App) (wasmkeeper.Keeper, error) {
+	// The store key for WASM
+	storeKey := app.GetKey(wasmtypes.StoreKey)
+	if storeKey == nil {
+		return wasmkeeper.Keeper{}, fmt.Errorf("wasm store key not found")
+	}
+
+	// Build a store service from that key
+	storeService := runtime.NewKVStoreService(storeKey)
+	wasmConfig := wasmtypes.DefaultWasmConfig()
+
+	// Typically the "authority" is the x/gov module address
+	authority := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+
+	// Capabilities string: can be "iterator,staking", or "all", etc.
+	availableCapabilities := "iterator,staking"
+
+	// Construct the keeper with minimal stubs for advanced features
+	k := wasmkeeper.NewKeeper(
+		app.appCodec,
+		storeService,
+		app.AccountKeeper,
+		app.BankKeeper,
+		*app.StakingKeeper,
+		nil, // DistributionKeeper
+		nil, // ICS4Wrapper
+		nil, // ChannelKeeper
+		nil, // PortKeeper
+		nil, // CapabilityKeeper
+		nil, // ICS20TransferPortSource
+		nil, // MessageRouter
+		nil, // GRPCQueryRouter
+		filepath.Join(DefaultNodeHome, "wasm"),
+		wasmConfig,
+		availableCapabilities,
+		authority,
+	)
+	return k, nil
+}
+
+// AppConfig provides the app config using depinject
 func AppConfig() depinject.Config {
 	return depinject.Configs(
 		appConfig,
-		// Loads the app config from a YAML file.
-		// appconfig.LoadYAML(AppConfigYAML),
+		// If you have a YAML config, uncomment and call: appconfig.LoadYAML(AppConfigYAML)
 		depinject.Supply(
-			// supply custom module basics
 			map[string]module.AppModuleBasic{
 				genutiltypes.ModuleName: genutil.NewAppModuleBasic(genutiltypes.DefaultMessageValidator),
 				govtypes.ModuleName:     gov.NewAppModuleBasic(getGovProposalHandlers()),
-				// this line is used by starport scaffolding # stargate/appConfig/moduleBasic
 			},
 		),
 	)
 }
 
-// New returns a reference to an initialized App.
+// New initializes and returns a reference to your App
 func New(
 	logger log.Logger,
 	db dbm.DB,
@@ -204,64 +240,20 @@ func New(
 	var (
 		app        = &App{}
 		appBuilder *runtime.AppBuilder
-
-		// merge the AppConfig and other configuration in one config
+		// merges config in one
 		appConfig = depinject.Configs(
 			AppConfig(),
 			depinject.Supply(
-				// Supply the application options
+				// supply the application options and a few custom callbacks
 				appOpts,
-				// Supply with IBC keeper getter for the IBC modules with App Wiring.
-				// The IBC Keeper cannot be passed because it has not been initiated yet.
-				// Passing the getter, the app IBC Keeper will always be accessible.
-				// This needs to be removed after IBC supports App Wiring.
 				app.GetIBCKeeper,
 				app.GetCapabilityScopedKeeper,
-				// Supply the logger
 				logger,
-
-				// ADVANCED CONFIGURATION
-				//
-				// AUTH
-				//
-				// For providing a custom function required in auth to generate custom account types
-				// add it below. By default the auth module uses simulation.RandomGenesisAccounts.
-				//
-				// authtypes.RandomGenesisAccountsFn(simulation.RandomGenesisAccounts),
-				//
-				// For providing a custom a base account type add it below.
-				// By default the auth module uses authtypes.ProtoBaseAccount().
-				//
-				// func() sdk.AccountI { return authtypes.ProtoBaseAccount() },
-				//
-				// For providing a different address codec, add it below.
-				// By default the auth module uses a Bech32 address codec,
-				// with the prefix defined in the auth module configuration.
-				//
-				// func() address.Codec { return <- custom address codec type -> }
-
-				//
-				// STAKING
-				//
-				// For provinding a different validator and consensus address codec, add it below.
-				// By default the staking module uses the bech32 prefix provided in the auth config,
-				// and appends "valoper" and "valcons" for validator and consensus addresses respectively.
-				// When providing a custom address codec in auth, custom address codecs must be provided here as well.
-				//
-				// func() runtime.ValidatorAddressCodec { return <- custom validator address codec type -> }
-				// func() runtime.ConsensusAddressCodec { return <- custom consensus address codec type -> }
-
-				//
-				// MINT
-				//
-
-				// For providing a custom inflation function for x/mint add here your
-				// custom function that implements the minttypes.InflationCalculationFn
-				// interface.
 			),
 		)
 	)
 
+	// Depinject all keepers except the WasmKeeper
 	if err := depinject.Inject(appConfig,
 		&appBuilder,
 		&app.appCodec,
@@ -286,80 +278,56 @@ func New(
 		&app.GroupKeeper,
 		&app.CircuitBreakerKeeper,
 		&app.InferenceKeeper,
-		// this line is used by starport scaffolding # stargate/app/keeperDefinition
 	); err != nil {
 		panic(err)
 	}
 
-	// Below we could construct and set an application specific mempool and
-	// ABCI 1.0 PrepareProposal and ProcessProposal handlers. These defaults are
-	// already set in the SDK's BaseApp, this shows an example of how to override
-	// them.
-	//
-	// Example:
-	//
-	// app.App = appBuilder.Build(...)
-	// nonceMempool := mempool.NewSenderNonceMempool()
-	// abciPropHandler := NewDefaultProposalHandler(nonceMempool, app.App.BaseApp)
-	//
-	// app.App.BaseApp.SetMempool(nonceMempool)
-	// app.App.BaseApp.SetPrepareProposal(abciPropHandler.PrepareProposalHandler())
-	// app.App.BaseApp.SetProcessProposal(abciPropHandler.ProcessProposalHandler())
-	//
-	// Alternatively, you can construct BaseApp options, append those to
-	// baseAppOptions and pass them to the appBuilder.
-	//
-	// Example:
-	//
-	// prepareOpt = func(app *baseapp.BaseApp) {
-	// 	abciPropHandler := baseapp.NewDefaultProposalHandler(nonceMempool, app)
-	// 	app.SetPrepareProposal(abciPropHandler.PrepareProposalHandler())
-	// }
-	// baseAppOptions = append(baseAppOptions, prepareOpt)
-	//
-	// create and set vote extension handler
-	// voteExtOp := func(bApp *baseapp.BaseApp) {
-	// 	voteExtHandler := NewVoteExtensionHandler()
-	// 	voteExtHandler.SetHandlers(bApp)
-	// }
-
+	// Build the base application
 	app.App = appBuilder.Build(db, traceStore, baseAppOptions...)
 
-	// Register legacy modules
+	// Manually provide the WASM keeper
+	wasmK, err := ProvideWasmKeeper(app)
+	if err != nil {
+		return nil, err
+	}
+	app.WasmKeeper = wasmK
+
+	// If you have custom IBC modules to register, do so
 	if err := app.registerIBCModules(appOpts); err != nil {
 		return nil, err
 	}
 
-	// register streaming services
+	// Register streaming if needed
 	if err := app.RegisterStreamingServices(appOpts, app.kvStoreKeys()); err != nil {
 		return nil, err
 	}
 
-	/****  Module Options ****/
-
+	// Register invariants
 	app.ModuleManager.RegisterInvariants(app.CrisisKeeper)
 
-	// create the simulation manager and define the order of the modules for deterministic simulations
-	//
-	// NOTE: this is not required apps that don't use the simulator for fuzz testing transactions
+	// Create a simulation manager
 	overrideModules := map[string]module.AppModuleSimulation{
-		authtypes.ModuleName: auth.NewAppModule(app.appCodec, app.AccountKeeper, authsims.RandomGenesisAccounts, app.GetSubspace(authtypes.ModuleName)),
+		authtypes.ModuleName: auth.NewAppModule(
+			app.appCodec,
+			app.AccountKeeper,
+			authsims.RandomGenesisAccounts,
+			app.GetSubspace(authtypes.ModuleName),
+		),
 	}
 	app.sm = module.NewSimulationManagerFromAppModules(app.ModuleManager.Modules, overrideModules)
 	app.sm.RegisterStoreDecoders()
 
+	// Setup upgrade handlers if needed
 	app.setupUpgradeHandlers()
-	// A custom InitChainer can be set if extra pre-init-genesis logic is required.
-	// By default, when using app wiring enabled module, this is not required.
-	// For instance, the upgrade module will set automatically the module version map in its init genesis thanks to app wiring.
-	// However, when registering a module manually (i.e. that does not support app wiring), the module version map
-	// must be set manually as follow. The upgrade module will de-duplicate the module version map.
-	//
+
+	// Optionally override InitChainer if needed
+	// e.g.:
 	// app.SetInitChainer(func(ctx sdk.Context, req *abci.RequestInitChain) (*abci.ResponseInitChain, error) {
-	// 	app.UpgradeKeeper.SetModuleVersionMap(ctx, app.ModuleManager.GetVersionMap())
-	// 	return app.App.InitChainer(ctx, req)
+	//     app.UpgradeKeeper.SetModuleVersionMap(ctx, app.ModuleManager.GetVersionMap())
+	//     return app.App.InitChainer(ctx, req)
 	// })
 
+	// Finally, load the chain state
 	if err := app.Load(loadLatest); err != nil {
 		return nil, err
 	}
@@ -367,90 +335,80 @@ func New(
 	return app, nil
 }
 
-// LegacyAmino returns App's amino codec.
-//
-// NOTE: This is solely to be used for testing purposes as it may be desirable
-// for modules to register their own custom testing types.
+// LegacyAmino returns the application's amino codec
 func (app *App) LegacyAmino() *codec.LegacyAmino {
 	return app.legacyAmino
 }
 
-// AppCodec returns App's app codec.
-//
-// NOTE: This is solely to be used for testing purposes as it may be desirable
-// for modules to register their own custom testing types.
+// AppCodec returns the application's codec
 func (app *App) AppCodec() codec.Codec {
 	return app.appCodec
 }
 
-// GetKey returns the KVStoreKey for the provided store key.
+// GetKey returns a KVStoreKey from the runtime store
 func (app *App) GetKey(storeKey string) *storetypes.KVStoreKey {
-	kvStoreKey, ok := app.UnsafeFindStoreKey(storeKey).(*storetypes.KVStoreKey)
+	kv, ok := app.UnsafeFindStoreKey(storeKey).(*storetypes.KVStoreKey)
 	if !ok {
 		return nil
 	}
-	return kvStoreKey
+	return kv
 }
 
-// GetMemKey returns the MemoryStoreKey for the provided store key.
+// GetMemKey returns a MemoryStoreKey from the runtime store
 func (app *App) GetMemKey(storeKey string) *storetypes.MemoryStoreKey {
-	key, ok := app.UnsafeFindStoreKey(storeKey).(*storetypes.MemoryStoreKey)
+	mk, ok := app.UnsafeFindStoreKey(storeKey).(*storetypes.MemoryStoreKey)
 	if !ok {
 		return nil
 	}
-
-	return key
+	return mk
 }
 
-// kvStoreKeys returns all the kv store keys registered inside App.
+// kvStoreKeys returns all KVStoreKey references
 func (app *App) kvStoreKeys() map[string]*storetypes.KVStoreKey {
-	keys := make(map[string]*storetypes.KVStoreKey)
+	out := make(map[string]*storetypes.KVStoreKey)
 	for _, k := range app.GetStoreKeys() {
 		if kv, ok := k.(*storetypes.KVStoreKey); ok {
-			keys[kv.Name()] = kv
+			out[kv.Name()] = kv
 		}
 	}
-
-	return keys
+	return out
 }
 
-// GetSubspace returns a param subspace for a given module name.
+// GetSubspace returns a param subspace for a given module
 func (app *App) GetSubspace(moduleName string) paramstypes.Subspace {
 	subspace, _ := app.ParamsKeeper.GetSubspace(moduleName)
 	return subspace
 }
 
-// GetIBCKeeper returns the IBC keeper.
+// GetIBCKeeper returns the IBC keeper
 func (app *App) GetIBCKeeper() *ibckeeper.Keeper {
 	return app.IBCKeeper
 }
 
-// GetCapabilityScopedKeeper returns the capability scoped keeper.
+// GetCapabilityScopedKeeper returns the scoped capability keeper for a module
 func (app *App) GetCapabilityScopedKeeper(moduleName string) capabilitykeeper.ScopedKeeper {
 	return app.CapabilityKeeper.ScopeToModule(moduleName)
 }
 
-// SimulationManager implements the SimulationApp interface.
+// SimulationManager returns the simulation manager
 func (app *App) SimulationManager() *module.SimulationManager {
 	return app.sm
 }
 
-// RegisterAPIRoutes registers all application module routes with the provided
-// API server.
+// RegisterAPIRoutes adds all application module routes to the provided API server
 func (app *App) RegisterAPIRoutes(apiSvr *api.Server, apiConfig config.APIConfig) {
 	app.App.RegisterAPIRoutes(apiSvr, apiConfig)
-	// register swagger API in app.go so that other applications can override easily
+
+	// register swagger
 	if err := server.RegisterSwaggerAPI(apiSvr.ClientCtx, apiSvr.Router, apiConfig.Swagger); err != nil {
 		panic(err)
 	}
 
-	// register app's OpenAPI routes.
+	// register openapi
 	docs.RegisterOpenAPIService(Name, apiSvr.Router)
 }
 
 // GetMaccPerms returns a copy of the module account permissions
-//
-// NOTE: This is solely to be used for testing purposes.
 func GetMaccPerms() map[string][]string {
 	dup := make(map[string][]string)
 	for _, perms := range moduleAccPerms {
@@ -459,17 +417,17 @@ func GetMaccPerms() map[string][]string {
 	return dup
 }
 
-// BlockedAddresses returns all the app's blocked account addresses.
+// BlockedAddresses returns a map of all blocked account addresses
 func BlockedAddresses() map[string]bool {
-	result := make(map[string]bool)
+	out := make(map[string]bool)
 	if len(blockAccAddrs) > 0 {
 		for _, addr := range blockAccAddrs {
-			result[addr] = true
+			out[addr] = true
 		}
 	} else {
 		for addr := range GetMaccPerms() {
-			result[addr] = true
+			out[addr] = true
 		}
 	}
-	return result
+	return out
 }
