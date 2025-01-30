d12e4c407289b18535a3ea8fcababa71d80e1254
dima <dima.orekhov@productscience.ai>
2025-01-30 00:33:33 -0800

    Implement get pricing endpoint

--- decentralized-api/api/pricing.go
+++ decentralized-api/api/pricing.go
@@ -19,18 +19,35 @@ func WrapPricing(cosmosClient cosmosclient.CosmosMessageClient) http.HandlerFunc
 }
 
 func getPricing(w http.ResponseWriter, cosmosClient cosmosclient.CosmosMessageClient) {
-
 	queryClient := cosmosClient.NewInferenceQueryClient()
 	req := &types.QueryCurrentEpochGroupDataRequest{}
-	response, err := queryClient.CurrentEpochGroupData(*cosmosClient.GetContext(), req)
+	context := *cosmosClient.GetContext()
+	response, err := queryClient.CurrentEpochGroupData(context, req)
 	if err != nil {
 		http.Error(w, "Failed to get current epoch group data", http.StatusInternalServerError)
 		return
 	}
+	unitOfComputePrice := response.EpochGroupData.UnitOfComputePrice
+
+	modelsResponse, err := queryClient.ModelsAll(context, &types.QueryModelsAllRequest{})
+	if err != nil {
+		http.Error(w, "Failed to get models", http.StatusInternalServerError)
+		return
+	}
+
+	models := make([]model.ModelPriceDto, len(modelsResponse.Model))
+	for _, m := range modelsResponse.Model {
+		pricePerToken := m.UnitOfComputePerToken * unitOfComputePrice
+		models = append(models, model.ModelPriceDto{
+			ModelId:                m.Id,
+			UnitsOfComputePerToken: m.UnitOfComputePerToken,
+			PricePerToken:          pricePerToken,
+		})
+	}
 
 	var responseBody = &model.PricingDto{
-		Price:  response.EpochGroupData.UnitOfComputePrice,
-		Models: []model.ModelPriceDto{},
+		Price:  unitOfComputePrice,
+		Models: models,
 	}
 
 	RespondWithJson(w, responseBody)
