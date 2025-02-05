f738b810d71010d65655705f392d8f158168d125
johnlong <john.long@productscience.ai>
2025-02-05 11:05:30 -0800

    Clean up logs a bit

--- inference-chain/x/inference/keeper/query_inference_participant.go
+++ inference-chain/x/inference/keeper/query_inference_participant.go
@@ -15,7 +15,6 @@ func (k Keeper) InferenceParticipant(goCtx context.Context, req *types.QueryInfe
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
-	k.LogInfo("InferenceParticipant", "address", req.Address)
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Process the query
@@ -24,17 +23,17 @@ func (k Keeper) InferenceParticipant(goCtx context.Context, req *types.QueryInfe
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid address")
 	}
-	k.LogInfo("InferenceParticipant address converted", "address", addr.String())
+	k.LogDebug("InferenceParticipant address converted", "address", addr.String())
 	acc := k.AccountKeeper.GetAccount(ctx, addr)
 	if acc == nil {
 		k.LogError("InferenceParticipant: Not Found", "address", req.Address)
 		return nil, status.Error(codes.NotFound, "account not found")
 	}
-	k.LogInfo("InferenceParticipant account found", "address", req.Address)
+	k.LogDebug("InferenceParticipant account found", "address", req.Address)
 	balance := k.bankView.SpendableCoin(ctx, addr, types.BaseCoin)
 
-	k.LogInfo("InferenceParticipant balance", "balance", balance)
-	k.LogInfo("InferenceParticipant pubkey", "pubkey", acc.GetPubKey().Bytes())
+	k.LogDebug("InferenceParticipant balance", "balance", balance)
+	k.LogDebug("InferenceParticipant pubkey", "pubkey", acc.GetPubKey().Bytes())
 	return &types.QueryInferenceParticipantResponse{
 		Pubkey:  base64.StdEncoding.EncodeToString(acc.GetPubKey().Bytes()),
 		Balance: balance.Amount.Int64(),
