9fe18b7e5681352e7c68a1afa1bc0d59c28cdf4b
dima <dima.orekhov@productscience.ai>
2025-02-06 21:35:40 -0800

    Implement task pool

--- inference-chain/x/inference/types/key_utils.go
+++ inference-chain/x/inference/types/key_utils.go
@@ -1,5 +1,7 @@
 package types
 
+import "strconv"
+
 func stringKey(id string) []byte {
 	var key []byte
 
@@ -8,4 +10,28 @@ func stringKey(id string) []byte {
 	key = append(key, []byte("/")...)
 
 	return key
+}
+
+func stringsKey(id1, id2 string) []byte {
+	var key []byte
+
+	id1Bytes := []byte(id1)
+	key = append(key, id1Bytes...)
+	key = append(key, []byte("/")...)
+
+	id2Bytes := []byte(id2)
+	key = append(key, id2Bytes...)
+	key = append(key, []byte("/")...)
+
+	return key
+}
+
+func intKey(id int64) []byte {
+	idStr := strconv.FormatInt(id, 10)
+	return stringKey(idStr)
+}
+
+func uintKey(id uint64) []byte {
+	idStr := strconv.FormatUint(id, 10)
+	return stringKey(idStr)
 }
