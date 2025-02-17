fca266cf292e9cde210f53667881e613611b5dd1
0xBECEDA <sakharovasonya1@gmail.com>
2025-02-17 11:01:05 +0100

    fix merge

--- test-snapshots.sh
+++ test-snapshots.sh
@@ -11,6 +11,7 @@ export RPC_SERVER_URL_1="http://join1-node:26657"
 export RPC_SERVER_URL_2="http://join2-node:26657"
 
 export SYNC_WITH_SNAPSHOTS=true
+
 export KEY_NAME=join3
 export NODE_CONFIG="node_payload_wiremock_${KEY_NAME}.json"
 export PORT=8083
