8356ad030b327e63c53e9fe2b5ad6e8ea3eb99fb
dima <dima.orekhov@productscience.ai>
2025-03-10 10:16:59 -0600

    Train config

--- decentralized-api/broker/node_client.go
+++ decentralized-api/broker/node_client.go
@@ -12,6 +12,7 @@ import (
 const (
 	trainStartPath  = "/api/v1/train/start"
 	trainStatusPath = "/api/v1/train/status"
+	stopPath        = "/api/v1/stop"
 )
 
 type InferenceNodeClient struct {
@@ -29,6 +30,63 @@ func NewNodeApi(node *apiconfig.InferenceNode) (*InferenceNodeClient, error) {
 			Timeout: 5 * time.Minute,
 		},
 	}, nil
+}
+
+type StartTraining struct {
+	TrainConfig TrainConfig `json:"train_config"`
+	TrainEnv    TrainEnv    `json:"train_env"`
+}
+
+type TrainConfig struct {
+	Project     string       `json:"project"`
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+	Group       string       `json:"group"`
+	Tags        []string     `json:"tags"`
+	Train       TrainParams  `json:"train"`
+	Data        DataConfig   `json:"data"`
+	Optim       OptimConfig  `json:"optim"`
+	Diloco      DilocoConfig `json:"diloco"`
+	Ckpt        Checkpoint   `json:"ckpt"`
+}
+
+type TrainParams struct {
+	MicroBatchSize int `json:"micro_bs"`
+	EvalInterval   int `json:"eval_interval"`
+}
+
+type DataConfig struct {
+	SeqLength int `json:"seq_length"`
+}
+
+type OptimConfig struct {
+	SchedType    string  `json:"sched_type"`
+	BatchSize    int     `json:"batch_size"`
+	WarmupSteps  int     `json:"warmup_steps"`
+	TotalSteps   int     `json:"total_steps"`
+	AdamBetas1   float64 `json:"adam_betas1"`
+	AdamBetas2   float64 `json:"adam_betas2"`
+	WeightDecay  float64 `json:"weight_decay"`
+	LearningRate float64 `json:"lr"`
+}
+
+type DilocoConfig struct {
+	InnerSteps int `json:"inner_steps"`
+}
+
+type Checkpoint struct {
+	Interval int    `json:"interval"`
+	TopK     int    `json:"topk"`
+	Path     string `json:"path"`
+}
+
+type TrainEnv struct {
+	GlobalAddr      string `json:"GLOBAL_ADDR"`
+	GlobalPort      string `json:"GLOBAL_PORT"`
+	GlobalRank      int    `json:"GLOBAL_RANK"`
+	GlobalUniqueID  int    `json:"GLOBAL_UNIQUE_ID"`
+	GlobalWorldSize int    `json:"GLOBAL_WORLD_SIZE"`
+	BasePort        int    `json:"BASE_PORT"`
 }
 
 func (api *InferenceNodeClient) StartTraining() error {
@@ -52,6 +110,20 @@ func (api *InferenceNodeClient) GetTrainingStatus() error {
 	}
 
 	_, err = utils.SendGetRequest(&api.client, requestUrl)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (api *InferenceNodeClient) Stop() error {
+	requestUrl, err := url.JoinPath(api.node.PoCUrl(), stopPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = utils.SendPostJsonRequest(&api.client, requestUrl, nil)
 	if err != nil {
 		return err
 	}
