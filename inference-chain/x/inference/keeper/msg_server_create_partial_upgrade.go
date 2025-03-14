cc97ef16cf120e1825a6d9526b5e6a3cae20f5c5
johnlong <john.long@productscience.ai>
2025-03-13 17:30:18 -0700

    Fix build

--- inference-chain/x/inference/keeper/msg_server_create_partial_upgrade.go
+++ inference-chain/x/inference/keeper/msg_server_create_partial_upgrade.go
@@ -9,8 +9,8 @@ import (
 )
 
 func (k msgServer) CreatePartialUpgrade(goCtx context.Context, msg *types.MsgCreatePartialUpgrade) (*types.MsgCreatePartialUpgradeResponse, error) {
-	if k.GetAuthority() != msg.Authority {
-		return nil, errorsmod.Wrapf(types.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+	if k.GetAuthority() != msg.Creator {
+		return nil, errorsmod.Wrapf(types.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), msg.Creator)
 	}
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
