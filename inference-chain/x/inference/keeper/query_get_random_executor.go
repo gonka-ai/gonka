5e97c6da913fe20d3187181c60c1a30871ac9b84
dima <dima.orekhov@productscience.ai>
2025-03-20 19:20:46 -0600

    Get random member for inference accounting for a model

--- inference-chain/x/inference/keeper/query_get_random_executor.go
+++ inference-chain/x/inference/keeper/query_get_random_executor.go
@@ -2,9 +2,11 @@ package keeper
 
 import (
 	"context"
+	"github.com/cosmos/cosmos-sdk/x/group"
 	"github.com/productscience/inference/x/inference/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"slices"
 )
 
 func (k Keeper) GetRandomExecutor(goCtx context.Context, req *types.QueryGetRandomExecutorRequest) (*types.QueryGetRandomExecutorResponse, error) {
@@ -16,7 +18,28 @@ func (k Keeper) GetRandomExecutor(goCtx context.Context, req *types.QueryGetRand
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
-	participant, err := epochGroup.GetRandomMember(goCtx)
+
+	allParticipants := k.GetAllParticipant(goCtx)
+	participantsById := make(map[string]*types.Participant)
+	for _, participant := range allParticipants {
+		participantsById[participant.Address] = &participant
+	}
+
+	participant, err := epochGroup.GetRandomMember(goCtx, func(members []*group.GroupMember) []*group.GroupMember {
+		filteredMembers := make([]*group.GroupMember, 0)
+		for _, member := range members {
+			participant, ok := participantsById[member.Member.Address]
+			if !ok || participant == nil {
+				continue
+			}
+
+			if slices.Contains(participant.Models, req.Model) {
+				filteredMembers = append(filteredMembers, member)
+			}
+		}
+
+		return filteredMembers
+	})
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
