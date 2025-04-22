4afd313aa3cdab801ef08f19858ae2713d44718c
dima <dima.orekhov@productscience.ai>
2025-04-21 21:17:38 -0700

    copy_proto.sh v2

--- copy_proto.sh
+++ copy_proto.sh
@@ -1,1 +1,1 @@
-cp ../chain-protos/proto/network_node/v1/network_node.proto inference-chain/proto/network_node/v1/network_node.proto
+cp ../chain-protos/proto/network_node/v1/network_node.proto inference-chain/proto/inference/inference/network_node.proto
