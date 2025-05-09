c80ebccbe94aec8a58660a498b14bbe0fa3d1f35
Gleb Morgachev <morgachev.g@gmail.com>
2025-05-08 22:51:01 -0700

    New chain id!

--- inference-chain/scripts/init-docker-genesis.sh
+++ inference-chain/scripts/init-docker-genesis.sh
@@ -12,7 +12,7 @@ echo "KEYRING_BACKEND: $KEYRING_BACKEND"
 
 KEY_NAME="genesis"
 APP_NAME="inferenced"
-CHAIN_ID="gonka-testnet-1"
+CHAIN_ID="gonka-testnet-2"
 COIN_DENOM="nicoin"
 STATE_DIR="/root/.inference"
 # Init the chain:
