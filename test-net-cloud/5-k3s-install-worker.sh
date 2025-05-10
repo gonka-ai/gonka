1b31c71bebdaa96cef50e59821d047d5f919bf41
dima <dima.orekhov@productscience.ai>
2025-05-09 17:35:57 -0700

    k3s-install-worker

---
+++ test-net-cloud/5-k3s-install-worker.sh
@@ -0,0 +1,19 @@
+# Get token by running the command on the k8s-control-plane
+sudo cat /var/lib/rancher/k3s/server/node-token
+
+# Connet to the worker and run
+# Use the K3S_TOKEN obtained from the control plane.
+# K3S_URL points to the INTERNAL IP of the control plane.
+# --node-ip should be the INTERNAL IP of this worker node.
+# --flannel-iface should match your primary internal network interface (usually eth0 on GCP)
+#    to check use: ip -4 addr show
+CONTROL_PLANE_INTERNAL_IP="10.128.0.41"
+WORKER_INTERNAL_IP="10.128.0.42"
+curl -sfL https://get.k3s.io | K3S_URL=https://$CONTROL_PLANE_INTERNAL_IP:6443 \
+    K3S_TOKEN="$K3S_TOKEN" \
+    INSTALL_K3S_EXEC="--flannel-iface=ens4 \
+    --node-ip $WORKER_INTERNAL_IP \
+    --node-label 'nvidia.com/gpu=true'" sh - # Adding a label for GPU node
+
+# Check k3s-agent service status
+sudo systemctl status k3s-agent
