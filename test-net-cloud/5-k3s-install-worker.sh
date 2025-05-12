59e792dae2885094d154bd1f14a1e5c26bf9dc27
dima <dima.orekhov@productscience.ai>
2025-05-12 15:06:23 -0700

    install worker changes

--- test-net-cloud/5-k3s-install-worker.sh
+++ test-net-cloud/5-k3s-install-worker.sh
@@ -11,9 +11,9 @@ CONTROL_PLANE_INTERNAL_IP="10.128.0.41"
 WORKER_INTERNAL_IP="10.128.0.42"
 curl -sfL https://get.k3s.io | K3S_URL=https://$CONTROL_PLANE_INTERNAL_IP:6443 \
     K3S_TOKEN="$K3S_TOKEN" \
-    INSTALL_K3S_EXEC="--flannel-iface=ens4 \
+    INSTALL_K3S_EXEC="agent --flannel-iface=ens4 \
     --node-ip $WORKER_INTERNAL_IP \
-    --node-label 'nvidia.com/gpu=true'" sh - # Adding a label for GPU node
+    --node-label nvidia.com/gpu=true" sh -
 
 # Check k3s-agent service status
 sudo systemctl status k3s-agent
