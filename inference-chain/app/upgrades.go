b4f95647c4fcc70a2c8fca33f9565f1569163b6c
johnlong <john.long@productscience.ai>
2025-05-20 18:15:23 -0700

    Add upgrade for v0.1.4

--- inference-chain/app/upgrades.go
+++ inference-chain/app/upgrades.go
@@ -3,6 +3,7 @@
 package app
 
 import (
+	v0_1_4 "github.com/productscience/inference/app/upgrades/v0.1.4"
 	v1_1 "github.com/productscience/inference/app/upgrades/v1_1"
 	v2 "github.com/productscience/inference/app/upgrades/v2"
 )
@@ -10,4 +11,5 @@ import (
 func (app *App) setupUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(v2.UpgradeName, v2.CreateUpgradeHandler(app.ModuleManager, app.Configurator(), app.InferenceKeeper))
 	app.UpgradeKeeper.SetUpgradeHandler(v1_1.UpgradeName, v1_1.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
+	app.UpgradeKeeper.SetUpgradeHandler(v0_1_4.UpgradeName, v0_1_4.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 }
