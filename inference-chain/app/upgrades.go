fbee26192c8a21f2aa30ad4b0b2eb9f2c89e58a0
dima <dima.orekhov@productscience.ai>
2025-05-27 01:00:52 -0600

    register upgrade handler

--- inference-chain/app/upgrades.go
+++ inference-chain/app/upgrades.go
@@ -4,10 +4,12 @@ package app
 
 import (
 	v1_1 "github.com/productscience/inference/app/upgrades/v1_1"
+	"github.com/productscience/inference/app/upgrades/v1_4_test_update"
 	v2 "github.com/productscience/inference/app/upgrades/v2"
 )
 
 func (app *App) setupUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(v2.UpgradeName, v2.CreateUpgradeHandler(app.ModuleManager, app.Configurator(), app.InferenceKeeper))
 	app.UpgradeKeeper.SetUpgradeHandler(v1_1.UpgradeName, v1_1.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
+	app.UpgradeKeeper.SetUpgradeHandler(v1_4_test_update.UpgradeName, v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 }
