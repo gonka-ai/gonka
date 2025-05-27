2ec86ccb03f75e96e0d3659799c3ba0fd4d88bb1
dima <dima.orekhov@productscience.ai>
2025-05-27 01:00:52 -0600

    register upgrade handler
    # Conflicts:
    #	inference-chain/app/upgrades.go

--- inference-chain/app/upgrades.go
+++ inference-chain/app/upgrades.go
@@ -6,6 +6,7 @@ import (
 	v0_1_4 "github.com/productscience/inference/app/upgrades/v0.1.4"
 	v1_1 "github.com/productscience/inference/app/upgrades/v1_1"
 	"github.com/productscience/inference/app/upgrades/v1_8"
+	"github.com/productscience/inference/app/upgrades/v1_4_test_update"
 	v2 "github.com/productscience/inference/app/upgrades/v2"
 )
 
@@ -14,4 +15,5 @@ func (app *App) setupUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(v1_1.UpgradeName, v1_1.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 	app.UpgradeKeeper.SetUpgradeHandler(v0_1_4.UpgradeName, v0_1_4.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 	app.UpgradeKeeper.SetUpgradeHandler(v1_8.UpgradeName, v1_8.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
+	app.UpgradeKeeper.SetUpgradeHandler(v1_4_test_update.UpgradeName, v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 }
