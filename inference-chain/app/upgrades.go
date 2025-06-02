8431743a8f022a539321c89b806568bdc689c388
johnlong <john.long@productscience.ai>
2025-06-02 16:28:30 -0700

    bump

--- inference-chain/app/upgrades.go
+++ inference-chain/app/upgrades.go
@@ -11,10 +11,6 @@ import (
 func (app *App) setupUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(v2.UpgradeName, v2.CreateUpgradeHandler(app.ModuleManager, app.Configurator(), app.InferenceKeeper))
 	app.UpgradeKeeper.SetUpgradeHandler(v1_1.UpgradeName, v1_1.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
-	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-19", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
-	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-20", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
-	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-21", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
-	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-22", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
-	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-23", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-24", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
+	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-25", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 }
