d9b118618a56a7a5d96670e7097aaa5a4e642e67
johnlong <john.long@productscience.ai>
2025-06-06 12:12:51 -0700

    bump version

--- inference-chain/app/upgrades.go
+++ inference-chain/app/upgrades.go
@@ -17,4 +17,5 @@ func (app *App) setupUpgradeHandlers() {
 	app.UpgradeKeeper.SetUpgradeHandler(v1_8.UpgradeName, v1_8.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 	app.UpgradeKeeper.SetUpgradeHandler(v1_4_test_update.UpgradeName, v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-27", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
+	app.UpgradeKeeper.SetUpgradeHandler("v0.1.4-28", v1_4_test_update.CreateUpgradeHandler(app.ModuleManager, app.Configurator()))
 }
