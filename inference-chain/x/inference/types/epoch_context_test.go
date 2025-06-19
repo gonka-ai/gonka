0d772b1f64bf369e800b239f11a6bb0d35d89bf9
dima <dima.orekhov@productscience.ai>
2025-06-18 18:23:43 -0700

    abstract test

--- inference-chain/x/inference/types/epoch_context_test.go
+++ inference-chain/x/inference/types/epoch_context_test.go
@@ -18,100 +18,11 @@ func TestNilEpoch(t *testing.T) {
 		PocValidationDelay:    1,
 		PocValidationDuration: 10,
 	}
-	var i = int64(1)
-	for i < 10 {
-		ec := types.NewEpochContext(nil, epochParams, i)
-		require.Equal(t, uint64(0), ec.Epoch)
-		require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
-		require.Equal(t, -epochParams.EpochShift, ec.PocStartBlockHeight)
+	initialBlockHeight := int64(1)
+	startOfPoc := int64(10)
+	var initialEpochGroup *types.EpochGroupData = nil
 
-		requireNotAStageBoundary(t, ec, i)
-
-		i++
-	}
-
-	ec := types.NewEpochContext(nil, epochParams, i)
-	require.Equal(t, uint64(1), ec.Epoch)
-	require.Equal(t, types.PoCGeneratePhase, ec.GetCurrentPhase(i))
-	require.Equal(t, i, ec.PocStartBlockHeight)
-	require.True(t, ec.IsStartOfPoc(i))
-	startOfPoc := i
-
-	i++
-
-	for i < startOfPoc+epochParams.GetPoCWinddownStage() {
-		ec := types.NewEpochContext(nil, epochParams, i)
-		require.Equal(t, uint64(1), ec.Epoch)
-		require.Equal(t, types.PoCGeneratePhase, ec.GetCurrentPhase(i))
-		requireNotAStageBoundary(t, ec, i)
-
-		i++
-	}
-
-	valStart := startOfPoc + epochParams.GetStartOfPoCValidationStage()
-	for i < valStart {
-		ec := types.NewEpochContext(nil, epochParams, i)
-		require.Equal(t, uint64(1), ec.Epoch)
-		require.Equal(t, types.PoCGenerateWindDownPhase, ec.GetCurrentPhase(i))
-
-		if i == startOfPoc+epochParams.GetEndOfPoCStage() {
-			require.True(t, ec.IsEndOfPoCStage(i))
-		} else {
-			requireNotAStageBoundary(t, ec, i)
-		}
-
-		i++
-	}
-
-	// Validation phase starts
-	ec = types.NewEpochContext(nil, epochParams, i)
-	require.Equal(t, uint64(1), ec.Epoch)
-	require.Equal(t, types.PoCValidatePhase, ec.GetCurrentPhase(i))
-	require.True(t, ec.IsStartOfPoCValidationStage(i))
-	i++
-
-	for i < startOfPoc+epochParams.GetPoCValidationWindownStage() {
-		ec = types.NewEpochContext(nil, epochParams, i)
-		require.Equal(t, uint64(1), ec.Epoch)
-		require.Equal(t, types.PoCValidatePhase, ec.GetCurrentPhase(i))
-
-		requireNotAStageBoundary(t, ec, i)
-
-		i++
-	}
-
-	for i < startOfPoc+epochParams.GetEndOfPoCValidationStage() {
-		ec = types.NewEpochContext(nil, epochParams, i)
-		require.Equal(t, uint64(1), ec.Epoch)
-		require.Equal(t, types.PoCValidateWindDownPhase, ec.GetCurrentPhase(i))
-
-		requireNotAStageBoundary(t, ec, i)
-
-		i++
-	}
-
-	ec = types.NewEpochContext(nil, epochParams, i)
-	require.Equal(t, uint64(1), ec.Epoch)
-	require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
-	require.True(t, ec.IsEndOfPoCValidationStage(i))
-	i++
-
-	ec = types.NewEpochContext(nil, epochParams, i)
-	require.Equal(t, uint64(1), ec.Epoch)
-	require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
-	require.True(t, ec.IsSetNewValidatorsStage(i))
-	i++
-
-	assert.Panics(t, func() {
-		fmt.Println("About to call NewEpochContext")
-		types.NewEpochContext(nil, epochParams, i)
-		fmt.Println("Returned from NewEpochContext (no panic?)")
-	})
-
-	ec = types.NewEpochContext(&types.EpochGroupData{EpochGroupId: 1, PocStartBlockHeight: uint64(startOfPoc)}, epochParams, i)
-	require.Equal(t, uint64(1), ec.Epoch)
-	require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
-	require.True(t, ec.IsClaimMoneyStage(i))
+	test(t, epochParams, initialBlockHeight, startOfPoc, initialEpochGroup)
 }
 
 func requireNotAStageBoundary(t *testing.T, ec *types.EpochContext, i int64) {
@@ -152,4 +63,108 @@ func Test(t *testing.T) {
 
 		i++
 	}
+}
+
+func getEpochId(initialEpochGroup *types.EpochGroupData) uint64 {
+	if initialEpochGroup == nil {
+		return 0
+	} else {
+		return initialEpochGroup.EpochGroupId
+	}
+}
+
+func test(t *testing.T, epochParams types.EpochParams, initialBlockHeight int64, startOfPoc int64, initialEpochGroup *types.EpochGroupData) {
+	var i = initialBlockHeight
+	for i < startOfPoc {
+		ec := types.NewEpochContext(initialEpochGroup, epochParams, i)
+		require.Equal(t, getEpochId(initialEpochGroup), ec.Epoch)
+		require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
+		require.Equal(t, -epochParams.EpochShift, ec.PocStartBlockHeight)
+
+		requireNotAStageBoundary(t, ec, i)
+
+		i++
+	}
+
+	ec := types.NewEpochContext(initialEpochGroup, epochParams, i)
+	require.Equal(t, uint64(1), ec.Epoch)
+	require.Equal(t, types.PoCGeneratePhase, ec.GetCurrentPhase(i))
+	require.Equal(t, i, ec.PocStartBlockHeight)
+	require.True(t, ec.IsStartOfPoc(i))
+
+	i++
+
+	for i < startOfPoc+epochParams.GetPoCWinddownStage() {
+		ec := types.NewEpochContext(initialEpochGroup, epochParams, i)
+		require.Equal(t, uint64(1), ec.Epoch)
+		require.Equal(t, types.PoCGeneratePhase, ec.GetCurrentPhase(i))
+		requireNotAStageBoundary(t, ec, i)
+
+		i++
+	}
+
+	valStart := startOfPoc + epochParams.GetStartOfPoCValidationStage()
+	for i < valStart {
+		ec := types.NewEpochContext(initialEpochGroup, epochParams, i)
+		require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+		require.Equal(t, types.PoCGenerateWindDownPhase, ec.GetCurrentPhase(i))
+
+		if i == startOfPoc+epochParams.GetEndOfPoCStage() {
+			require.True(t, ec.IsEndOfPoCStage(i))
+		} else {
+			requireNotAStageBoundary(t, ec, i)
+		}
+
+		i++
+	}
+
+	// Validation phase starts
+	ec = types.NewEpochContext(initialEpochGroup, epochParams, i)
+	require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+	require.Equal(t, types.PoCValidatePhase, ec.GetCurrentPhase(i))
+	require.True(t, ec.IsStartOfPoCValidationStage(i))
+	i++
+
+	for i < startOfPoc+epochParams.GetPoCValidationWindownStage() {
+		ec = types.NewEpochContext(initialEpochGroup, epochParams, i)
+		require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+		require.Equal(t, types.PoCValidatePhase, ec.GetCurrentPhase(i))
+
+		requireNotAStageBoundary(t, ec, i)
+
+		i++
+	}
+
+	for i < startOfPoc+epochParams.GetEndOfPoCValidationStage() {
+		ec = types.NewEpochContext(initialEpochGroup, epochParams, i)
+		require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+		require.Equal(t, types.PoCValidateWindDownPhase, ec.GetCurrentPhase(i))
+
+		requireNotAStageBoundary(t, ec, i)
+
+		i++
+	}
+
+	ec = types.NewEpochContext(initialEpochGroup, epochParams, i)
+	require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+	require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
+	require.True(t, ec.IsEndOfPoCValidationStage(i))
+	i++
+
+	ec = types.NewEpochContext(initialEpochGroup, epochParams, i)
+	require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+	require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
+	require.True(t, ec.IsSetNewValidatorsStage(i))
+	i++
+
+	assert.Panics(t, func() {
+		fmt.Println("About to call NewEpochContext")
+		types.NewEpochContext(initialEpochGroup, epochParams, i)
+		fmt.Println("Returned from NewEpochContext (no panic?)")
+	})
+
+	ec = types.NewEpochContext(&types.EpochGroupData{EpochGroupId: 1, PocStartBlockHeight: uint64(startOfPoc)}, epochParams, i)
+	require.Equal(t, getEpochId(initialEpochGroup)+1, ec.Epoch)
+	require.Equal(t, types.InferencePhase, ec.GetCurrentPhase(i))
+	require.True(t, ec.IsClaimMoneyStage(i))
 }
