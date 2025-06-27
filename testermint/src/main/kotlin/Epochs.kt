b01cc7db2f08f6e5bd0471361b9ddf8a21af8795
dima <dima.orekhov@productscience.ai>
2025-06-27 10:27:34 -0700

    Delete unused code

--- testermint/src/main/kotlin/Epochs.kt
+++ testermint/src/main/kotlin/Epochs.kt
@@ -1,8 +1,6 @@
 package com.productscience
 
-import com.productscience.data.EpochParams
 import com.productscience.data.EpochResponse
-import kotlin.math.truncate
 
 enum class EpochStage {
     START_OF_POC,
@@ -12,18 +10,6 @@ enum class EpochStage {
     END_OF_POC_VALIDATION,
     SET_NEW_VALIDATORS,
     CLAIM_REWARDS
-}
-
-fun EpochParams.shift(blockHeight: Long): Long = blockHeight + this.epochShift
-
-fun EpochParams.getStage(stage: EpochStage): Long = when (stage) {
-    EpochStage.START_OF_POC -> 0L
-    EpochStage.END_OF_POC -> getStage(EpochStage.START_OF_POC) + pocValidationDuration * epochMultiplier
-    EpochStage.POC_EXCHANGE_DEADLINE -> getStage(EpochStage.END_OF_POC) + pocExchangeDuration * epochMultiplier
-    EpochStage.START_OF_POC_VALIDATION -> getStage(EpochStage.END_OF_POC) + pocValidationDelay * epochMultiplier
-    EpochStage.END_OF_POC_VALIDATION -> getStage(EpochStage.START_OF_POC_VALIDATION) + pocValidationDuration * epochMultiplier
-    EpochStage.SET_NEW_VALIDATORS -> getStage(EpochStage.END_OF_POC_VALIDATION) + 1 * epochMultiplier
-    EpochStage.CLAIM_REWARDS -> getStage(EpochStage.SET_NEW_VALIDATORS) + 1 * epochMultiplier
 }
 
 fun EpochResponse.getNextStage(stage: EpochStage): Long {
