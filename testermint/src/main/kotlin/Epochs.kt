ed5e750754fa2374ad9a5e3955384148469f69b6
dima <dima.orekhov@productscience.ai>
2025-06-27 10:30:41 -0700

    fix naming

--- testermint/src/main/kotlin/Epochs.kt
+++ testermint/src/main/kotlin/Epochs.kt
@@ -14,20 +14,21 @@ enum class EpochStage {
 
 fun EpochResponse.getNextStage(stage: EpochStage): Long {
     return when (stage) {
-        EpochStage.START_OF_POC -> whateverIsNext(epochStages.pocStart, nextEpochStages.pocStart)
-        EpochStage.END_OF_POC -> whateverIsNext(epochStages.pocGenerationEnd, nextEpochStages.pocGenerationEnd)
-        EpochStage.POC_EXCHANGE_DEADLINE -> whateverIsNext(epochStages.pocExchangeWindow.end, nextEpochStages.pocExchangeWindow.end)
-        EpochStage.START_OF_POC_VALIDATION -> whateverIsNext(epochStages.pocValidationStart, nextEpochStages.pocValidationStart)
-        EpochStage.END_OF_POC_VALIDATION -> whateverIsNext(epochStages.pocValidationEnd, nextEpochStages.pocValidationEnd)
-        EpochStage.SET_NEW_VALIDATORS -> whateverIsNext(epochStages.setNewValidators, nextEpochStages.setNewValidators)
-        EpochStage.CLAIM_REWARDS -> whateverIsNext(epochStages.claimMoney, nextEpochStages.claimMoney)
+        EpochStage.START_OF_POC -> resolveUpcomingStage(epochStages.pocStart, nextEpochStages.pocStart)
+        EpochStage.END_OF_POC -> resolveUpcomingStage(epochStages.pocGenerationEnd, nextEpochStages.pocGenerationEnd)
+        EpochStage.POC_EXCHANGE_DEADLINE -> resolveUpcomingStage(epochStages.pocExchangeWindow.end, nextEpochStages.pocExchangeWindow.end)
+        EpochStage.START_OF_POC_VALIDATION -> resolveUpcomingStage(epochStages.pocValidationStart, nextEpochStages.pocValidationStart)
+        EpochStage.END_OF_POC_VALIDATION -> resolveUpcomingStage(epochStages.pocValidationEnd, nextEpochStages.pocValidationEnd)
+        EpochStage.SET_NEW_VALIDATORS -> resolveUpcomingStage(epochStages.setNewValidators, nextEpochStages.setNewValidators)
+        EpochStage.CLAIM_REWARDS -> resolveUpcomingStage(epochStages.claimMoney, nextEpochStages.claimMoney)
     }
 }
 
-fun EpochResponse.whateverIsNext(a: Long, b: Long): Long {
-    assert(a < b)
-    return when (blockHeight < a) {
-        true -> a
-        false -> b
+fun EpochResponse.resolveUpcomingStage(latestEpochStage: Long, nextEpochStage: Long): Long {
+    assert(latestEpochStage < nextEpochStage)
+    return if (blockHeight < latestEpochStage) {
+        latestEpochStage
+    } else {
+        nextEpochStage
     }
 }
