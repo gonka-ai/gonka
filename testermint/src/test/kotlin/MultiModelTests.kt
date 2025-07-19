9a8c6c05f07eae29ea10d8469cb3f6c8545f8eca
dima <dima.orekhov@productscience.ai>
2025-07-18 22:59:58 -0700

    wait more

--- testermint/src/test/kotlin/MultiModelTests.kt
+++ testermint/src/test/kotlin/MultiModelTests.kt
@@ -70,6 +70,7 @@ class MultiModelTests : TestermintTest() {
         var newState: InferencePayload
         do {
             logSection("Trying to get invalid inference. Tries left: $tries")
+            genesis.waitForNextInferenceWindow(5)
             newState = getInferenceValidationState(genesis, oddPair, newModelName)
         } while (newState.statusEnum != InferenceStatus.INVALIDATED && tries-- > 0)
         logSection("Verifying invalidation")
