00234e05f0597a4e7f56c0532cfc165353b74f31
Gleb Morgachev <gleb@productscience.ai>
2025-07-20 13:58:28 -0400

    Unordered transactions merge (#211)
    * Finishing the implementation, still tests to fix
    
    * Minor review fixes
    
    * For now, ALL signatures based on the original, unmodified prompt.
    That means we have to store BOTH prompts (one to repro correctly, one to verify signature). It's not ideal, especially if the payloads get really large, as they are wont to do in modern AI. We should look into optimizations in the future.
    
    * Fix consumer tests
    
    * Fix streaming tests
    
    * A little refactoring
    
    * WIP commit
    
    * Move to released version
    
    * Missing file
    
    * Add needed upgrade code.
    The big thing is to call RunMigrations (which we should have been). But that caused issues with the `capability` module, as it seems to have data on the chain despite having no module version logged.
    
    So, we fake that it already exists. It works, but I'm still not super happy about how.
    
    * Fix tests and unit tests
    
    * Added config parameters for the timeouts for our Inference requests/nonces
    
    * Enable unordered transactions
    Get rid of account sequence code.
    Get rid of mutex for sending transactions (allow parallel sending)
    Make stress test a little more robust and also harder to pass.
    
    * Handle MsgFinish after expiration
    
    ---------
    
    Co-authored-by: johnlong <john.long@productscience.ai>

--- testermint/src/test/kotlin/ValidationTests.kt
+++ testermint/src/test/kotlin/ValidationTests.kt
@@ -18,9 +18,20 @@ import java.util.concurrent.TimeUnit
 class ValidationTests : TestermintTest() {
     @Test
     fun `test valid in parallel`() {
-        val (_, genesis) = initCluster()
+        val (_, genesis) = initCluster(
+            config = inferenceConfig.copy(
+                genesisSpec = createSpec(
+                    epochLength = 100,
+                    epochShift = 80
+                )
+            ),
+            reboot = true
+        )
+
+        genesis.node.waitForMinimumBlock(35)
         logSection("Making inference requests in parallel")
-        val statuses = runParallelInferences(genesis, 100, maxConcurrentRequests = 100)
+        val requests = 50
+        val statuses = runParallelInferences(genesis, requests, maxConcurrentRequests = requests)
         Logger.info("Statuses: $statuses")
 
         logSection("Verifying inference statuses")
@@ -29,6 +40,7 @@ class ValidationTests : TestermintTest() {
         }).allMatch {
             it == InferenceStatus.VALIDATED || it == InferenceStatus.FINISHED
         }
+        assertThat(statuses).hasSize(requests)
 
         Thread.sleep(10000)
     }
@@ -128,7 +140,9 @@ fun runParallelInferences(
                 System.nanoTime()
                 // This works, because the Instant.now() resolution gives us 3 zeros at the end, so we know these will be unique
                 val timestamp = Instant.now().toEpochNanos() + i
-                genesis.makeInferenceRequest(inferenceRequestObject.copy(model = models.random()).toJson(), timestamp = timestamp)
+                val result = genesis.makeInferenceRequest(inferenceRequestObject.copy(model = models.random()).toJson(), timestamp = timestamp)
+                Logger.info("Result for $i: $result\n\n\n")
+                result
             } catch (e: Exception) {
                 Logger.error("Error making inference request: ${e.message}")
                 null
