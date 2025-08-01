70b73ea44b433969d55c64060a1d7939782783c7
DimaOrekhovPS <dima.orekhov@productscience.ai>
2025-07-31 17:26:14 -0700

    Internal test net "how to test" instruction (#241)
    * 3 blocks wait
    
    * DEL UNUSED ERR
    
    * testing instructions

--- test-net-cloud/compressa-testing/compressa-how-to.sh
+++ test-net-cloud/compressa-testing/compressa-how-to.sh
@@ -1,12 +1,12 @@
-# Download latest inferenced binary to create a dev account
-# Developer instruction:
-#  https://testnet.productscience.ai/developer/quickstart/
+# Define env vars
 export ACCOUNT_NAME="test-account"
 # Url of the genesis k8s node API
 export PLAIN_NODE_URL="http://34.9.136.116:30000/"
 export NODE_URL="http://34.9.136.116:30000/api"
+# Example, join node URL instead of genesis node URL:
 # export NODE_URL="http://34.9.136.116:30010"
 export GONKA_ENDPOINTS=$NODE_URL/v1
+# WARNING: redefine INFERENCED_BINARY to your local path to inferenced binary
 # export INFERENCED_BINARY="kubectl -n genesis exec node-0 -- inferenced"
 # export INFERENCED_BINARY="inferenced"
 export INFERENCED_BINARY="/Users/dima/cosmos/bin/inferenced"
@@ -15,11 +15,54 @@ export INFERENCED_BINARY="/Users/dima/cosmos/bin/inferenced"
 curl "$NODE_URL/v1/status" | jq
 curl "$NODE_URL/v1/epochs/current/participants" | jq
 
+# Simple test
+compressa-perf \
+	measure \
+	--node_url "$PLAIN_NODE_URL" \
+	--model_name Qwen/Qwen2.5-7B-Instruct \
+	--create-account-testnet \
+	--inferenced-path "$INFERENCED_BINARY" \
+	--experiment_name test \
+	--generate_prompts \
+	--num_prompts 3000 \
+	--prompt_length 3000 \
+	--num_tasks 200 \
+	--num_runners 100 \
+	--max_tokens 100
+
+# A continuous stess test, won't stop until you stop it manually
+compressa-perf \
+  stress \
+  --node_url "$PLAIN_NODE_URL" \
+  --model_name Qwen/Qwen2.5-7B-Instruct \
+  --create-account-testnet \
+  --inferenced-path "$INFERENCED_BINARY" \
+  --experiment_name "stress_test" \
+  --generate_prompts \
+  --num_prompts 2000 \
+  --prompt_length 10000 \
+  --num_runners 10 \
+  --max_tokens 300 \
+  --report_freq_min 1 \
+  --account-pool-size 10
+
+# To check balances run
+# It will create a .json file with balances in the compressa-testing/balances/ directory
+./check-balances.sh
+
+# You can also pass the address of the developer to the script
+export GONKA_ADDRESS
+./check-balances.sh
+
+# BELOW IS THE ADDITIONAL INFO and more examples,
+# it's not requried for basic testing outlined above!!!
+
+# Manually create a developer account:
 # Create account, should return 200
 "$INFERENCED_BINARY" create-client $ACCOUNT_NAME \
   --node-address "$NODE_URL"
 
-export GONKA_ADDRESS="gonka18u3gz9hawfx5pt8q4l0438e6xqrx67rfw9j89l"
+export GONKA_ADDRESS="gonka1mff5uudv6tuupxq2nvx8ezdt9c230kucxwqe5p"
 
 # View it
 "$INFERENCED_BINARY" keys list
@@ -31,7 +74,7 @@ export GONKA_ADDRESS="gonka18u3gz9hawfx5pt8q4l0438e6xqrx67rfw9j89l"
 GONKA_PRIVATE_KEY="$(echo y | "$INFERENCED_BINARY" keys export $ACCOUNT_NAME --unarmored-hex --unsafe)"
 echo "$GONKA_PRIVATE_KEY"
 
-# Use compressa:
+# Use compressa with a custom yaml comfig.
 # Prerequisite, create and activate venv:
 # 1. python3 -m venv compressa-venv
 # 2. source compressa-venv/bin/activate
@@ -69,20 +112,3 @@ kubectl -n genesis exec node-0 -- inferenced query bank balances gonka1mfyq5pe9z
 kubectl port-forward -n genesis svc/api 9200:9200
 
 kubectl port-forward -n join-k8s-worker-2 svc/api 9200:9200
-
-# len of prompt in symbols: 3000
-# tasks to be executed: 200
-# total parallel workers: 100
-compressa-perf \
-	measure \
-	--node_url "$PLAIN_NODE_URL" \
-	--model_name Qwen/Qwen2.5-7B-Instruct \
-	--create-account-testnet \
-	--inferenced-path "$INFERENCED_BINARY" \
-	--experiment_name test \
-	--generate_prompts \
-	--num_prompts 3000 \
-	--prompt_length 3000 \
-	--num_tasks 200 \
-	--num_runners 100 \
-	--max_tokens 100
