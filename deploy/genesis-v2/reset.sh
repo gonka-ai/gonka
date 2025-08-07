2c908617a245a6a0a05e5a1816179609ed9db436
dima <dima.orekhov@productscience.ai>
2025-08-07 11:18:19 -0700

    Explicitly state the platform

--- deploy/genesis-v2/reset.sh
+++ deploy/genesis-v2/reset.sh
@@ -1,2 +1,5 @@
 cp ../../inference-chain/scripts/init-docker-genesis.sh ./init-docker-genesis.sh
 rm -rf ../../multigen-tests/genesis
+
+# then run
+# docker compose -f docker-compose.yml up tmkms node
