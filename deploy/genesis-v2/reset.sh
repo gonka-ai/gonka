fd075ec27d510aa6ed70f08d736033ed92460083
dima <dima.orekhov@productscience.ai>
2025-08-07 11:23:25 -0700

    more debug commands

--- deploy/genesis-v2/reset.sh
+++ deploy/genesis-v2/reset.sh
@@ -2,4 +2,5 @@ cp ../../inference-chain/scripts/init-docker-genesis.sh ./init-docker-genesis.sh
 rm -rf ../../multigen-tests/genesis
 
 # then run
-# docker compose -f docker-compose.yml up tmkms node
+# docker compose -f docker-compose.yml up -d tmkms node
+# docker compose -f docker-compose.yml down
