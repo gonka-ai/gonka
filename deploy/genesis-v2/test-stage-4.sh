f0b29ac9e04b2bf89c2ba26d1067d808486fa191
dima <dima.orekhov@productscience.ai>
2025-08-08 11:25:23 -0700

    ...

--- deploy/genesis-v2/test-stage-4.sh
+++ deploy/genesis-v2/test-stage-4.sh
@@ -9,7 +9,7 @@ export BASE_DIR="./multigen-tests"
 # Number of validators to generate keys for
 NUM_VALIDATORS=${1:-3}
 
-rm -rf "$BASE_DIR/genesis-0/input-artifacts/gentx"
+rm -rf "$BASE_DIR/genesis-0/input-artifacts/gentx" || true
 mkdir -p "$BASE_DIR/genesis-0/input-artifacts/gentx"
 
 # Copy all gentx to genesis-0
@@ -26,7 +26,7 @@ export GENESIS_INDEX="0"
 export P2P_EXTERNAL_ADDRESS="$HOST_ACCESS_ADDR:$((26656 + 0 * 10))"
 ./stage-4-start.sh
 
-sleep 30
+sleep 50
 
 for i in $(seq 1 $(($NUM_VALIDATORS - 1))); do
   cp "$BASE_DIR/genesis-0/artifacts/genesis-final.json" "$BASE_DIR/genesis-$i/input-artifacts/genesis-final.json"
