2f2b9349dd2abefa5d7e523edbe288282c2a9ba9
johnlong <john.long@productscience.ai>
2025-08-19 15:29:08 -0700

    Move keys to keys.

--- inference-chain/x/inference/types/key_dynamic_pricing.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "cosmossdk.io/collections"
-
-// Collections prefixes for dynamic pricing storage
-var (
-	DynamicPricingCurrentPrefix  = collections.NewPrefix(4)
-	DynamicPricingCapacityPrefix = collections.NewPrefix(5)
-)
