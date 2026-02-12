299943bf3e429589340756f9cb283eca41719b90
e.evdomashenko <e.evdomashenko@sapiens.solutions>
2026-02-12 20:05:31 +0300

    fix: reorder validation checks

--- decentralized-api/internal/server/public/errors.go
+++ decentralized-api/internal/server/public/errors.go
@@ -7,9 +7,10 @@ import (
 )
 
 var (
-	ErrRequestAuth         = echo.NewHTTPError(http.StatusUnauthorized, "Authorization is required")
-	ErrAccountNotFound     = echo.NewHTTPError(http.StatusNotFound, "Account not found")
-	ErrInsufficientBalance = echo.NewHTTPError(http.StatusPaymentRequired, "Insufficient balance")
+	ErrRequestAuth                  = echo.NewHTTPError(http.StatusUnauthorized, "Authorization is required")
+	ErrAccountNotFound              = echo.NewHTTPError(http.StatusNotFound, "Account not found")
+	ErrInsufficientBalance          = echo.NewHTTPError(http.StatusPaymentRequired, "Insufficient balance")
+	ErrUnsupportedMessageContent    = echo.NewHTTPError(http.StatusBadRequest, "Unsupported message content")
 
 	ErrIdRequired        = echo.NewHTTPError(http.StatusBadRequest, "Id is required")
 	ErrAddressRequired   = echo.NewHTTPError(http.StatusBadRequest, "Address is required")
