1ac1fe9e710c6eef33a5acacd5eff35a243b5052
e.evdomashenko <e.evdomashenko@sapiens.solutions>
2026-03-12 11:23:15 +0300

    fix: added CheckPermission

--- inference-chain/x/inference/keeper/msg_server_delete_governance_model.go
+++ inference-chain/x/inference/keeper/msg_server_delete_governance_model.go
@@ -3,14 +3,13 @@ package keeper
 import (
 	"context"
 
-	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/productscience/inference/x/inference/types"
 )
 
 func (k msgServer) DeleteGovernanceModel(goCtx context.Context, msg *types.MsgDeleteGovernanceModel) (*types.MsgDeleteGovernanceModelResponse, error) {
-	if k.GetAuthority() != msg.Authority {
-		return nil, errorsmod.Wrapf(types.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.GetAuthority(), msg.Authority)
+	if err := k.CheckPermission(goCtx, msg, GovernancePermission); err != nil {
+		return nil, err
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
