8b911b19cc24ec797ec91180c0d965f179577293
Gleb Morgachev <morgachev.g@gmail.com>
2026-04-25 22:15:00 +0000

    disable gas price

--- inference-chain/app/upgrades/v0_2_12/upgrades.go
+++ inference-chain/app/upgrades/v0_2_12/upgrades.go
@@ -781,6 +781,7 @@ func setFeeParams(ctx context.Context, k keeper.Keeper) error {
 	}
 
 	fp := types.DefaultFeeParams()
+	fp.MinGasPriceNgonka = 0
 	params.FeeParams = fp
 	if err := k.SetParams(ctx, params); err != nil {
 		k.LogError("failed to set fee params during upgrade", types.Upgrades, "error", err)
