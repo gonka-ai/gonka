9aa685274feb19936c0ef5b09f9bcedd288eac35
Gleb Morgachev <morgachev.g@gmail.com>
2026-04-26 04:46:32 +0000

    fee params

--- inference-chain/app/upgrades/v0_2_12/upgrades.go
+++ inference-chain/app/upgrades/v0_2_12/upgrades.go
@@ -781,6 +781,7 @@ func setFeeParams(ctx context.Context, k keeper.Keeper) error {
 	}
 
 	fp := types.DefaultFeeParams()
+	// Note: temporary due to issue in gas estimations.
 	fp.MinGasPriceNgonka = 0
 	params.FeeParams = fp
 	if err := k.SetParams(ctx, params); err != nil {
